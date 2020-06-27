583afe4cf466b844a50a74d536d0abf35d1a1a2c
Madpeter <madpeter@gmail.com>
2020-06-27 12:57:11 +0100

    Redirects to render_list

--- public_html/site/view/stream/with_status.php
+++ public_html/site/view/stream/with_status.php
@@ -7,33 +7,5 @@ $rental_set = new rental_set();
 $rental_set->load_ids($stream_set->get_all_by_field("rentallink"));
 $rental_set_ids = $rental_set->get_all_ids();
 
-$table_head = array("id","UID","Server","Port","Status");
-$table_body = array();
-
-foreach($stream_set->get_all_ids() as $streamid)
-{
-    $stream = $stream_set->get_object_by_id($streamid);
-    $server = $server_set->get_object_by_id($stream->get_serverlink());
-    $entry = array();
-    $entry[] = $stream->get_id();
-    $entry[] = '<a href="[[url_base]]stream/manage/'.$stream->get_stream_uid().'">'.$stream->get_stream_uid().'</a>';
-    $entry[] = $server->get_domain();
-    $entry[] = $stream->get_port();
-    if($stream->get_needwork() == false)
-    {
-        if($stream->get_rentallink() != null)
-        {
-            if(in_array($stream->get_rentallink(),$rental_set_ids) == true)
-            {
-                $rental = $rental_set->get_object_by_id($stream->get_rentallink());
-                $entry[] = '<a href="[[url_base]]client/manage/'.$rental->get_rental_uid().'">Sold</a>';
-            }
-            else $entry[] = "Rented but cant find rental.";
-        }
-        else $entry[] = "Ready";
-    }
-    else $entry[] = "Need work";
-    $table_body[] = $entry;
-}
-echo render_datatable($table_head,$table_body);
+include("site/view/stream/render_list.php");
 ?>
