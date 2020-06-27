6dc6dd07a37eaccaf976a253677a8c38de0e0a37
Madpeter <madpeter@gmail.com>
2020-06-27 10:33:33 +0100

    Adds support for full list of clients plus number of renewals

--- public_html/site/view/client/default.php
+++ public_html/site/view/client/default.php
@@ -1,22 +1,29 @@
 <?php
-$template_parts["page_title"] .= " Select a notice level";
-$notice_set = new notice_set();
-$notice_set->loadAll();
-$rental = new rental();
-$group_count = $sql->group_count($rental->get_table(),"noticelink");
-$table_head = array("id","NoticeLevel","Count");
-$table_body = array();
-if($group_count["status"] == true)
+if($slconfig->get_clients_list_mode() == true)
 {
-    foreach($group_count["dataset"] as $key => $count)
-    {
-        $notice = $notice_set->get_object_by_id($key);
-        $entry = array();
-        $entry[] = $notice->get_id();
-        $entry[] = '<a href="[[url_base]]client/bynoticelevel/'.$notice->get_id().'">'.$notice->get_name().'</a>';
-        $entry[] = $count;
-        $table_body[] = $entry;
-    }
+    include("site/view/client/list.php");
 }
-echo render_datatable($table_head,$table_body);
+else
+{
+    $template_parts["page_title"] .= " Select a notice level";
+    $notice_set = new notice_set();
+    $notice_set->loadAll();
+    $rental = new rental();
+    $group_count = $sql->group_count($rental->get_table(),"noticelink");
+    $table_head = array("id","NoticeLevel","Count");
+    $table_body = array();
+    if($group_count["status"] == true)
+    {
+        foreach($group_count["dataset"] as $key => $count)
+        {
+            $notice = $notice_set->get_object_by_id($key);
+            $entry = array();
+            $entry[] = $notice->get_id();
+            $entry[] = '<a href="[[url_base]]client/bynoticelevel/'.$notice->get_id().'">'.$notice->get_name().'</a>';
+            $entry[] = $count;
+            $table_body[] = $entry;
+        }
+    }
+    echo render_datatable($table_head,$table_body);
+}
 ?>
--- public_html/site/view/client/list.php
+++ public_html/site/view/client/list.php
@@ -1,17 +1,22 @@
 <?php
-$template_parts["page_actions"] = "";
 $rental_set = new rental_set();
-$rental_set->load_with_config($whereconfig);
+$rental_set->loadAll();
+
 $avatar_set = new avatar_set();
 $avatar_set->load_ids($rental_set->get_all_by_field("avatarlink"));
+
 $stream_set = new stream_set();
 $stream_set->load_ids($rental_set->get_all_by_field("streamlink"));
-$package_set = new package_set();
-$package_set->load_ids($stream_set->get_all_by_field("packagelink"));
+
 $notice_set = new notice_set();
 $notice_set->load_ids($rental_set->get_all_by_field("noticelink"));
 
-$table_head = array("id","Rental UID","Avatar","Package","Port","Timeleft","NoticeLevel","Renewals");
+$package_set = new package_set();
+$package_set->load_ids($stream_set->get_all_by_field("packagelink"));
+
+$template_parts["page_title"] .= " [All]";
+
+$table_head = array("id","Rental UID","Avatar","Package","Port","Timeleft/Expired","Notice level","Renewals");
 $table_body = array();
 
 foreach($rental_set->get_all_ids() as $rental_id)
@@ -24,11 +29,19 @@ foreach($rental_set->get_all_ids() as $rental_id)
     $entry = array();
     $entry[] = $rental->get_id();
     $entry[] = '<a href="[[url_base]]client/manage/'.$rental->get_rental_uid().'">'.$rental->get_rental_uid().'</a>';
-    $entry[] = explode(" ",$avatar->get_avatarname())[0];
-    if($package == null) $entry[] = "Err";
-    else $entry[] = $package->get_name();
+    $av_detail = explode(" ",$avatar->get_avatarname());
+    if($av_detail[1] != "Resident") $entry[] = $avatar->get_avatarname();
+    else $entry[] = $av_detail[0];
+    $entry[] = $package->get_name();
     $entry[] = $stream->get_port();
-    $entry[] = timeleft_hours_and_days($rental->get_expireunixtime());
+    if($rental->get_expireunixtime() > time())
+    {
+        $entry[] = "Active - ".timeleft_hours_and_days($rental->get_expireunixtime());
+    }
+    else
+    {
+        $entry[] = "Expired - ".expired_ago($rental->get_expireunixtime());
+    }
     $entry[] = $notice->get_name();
     $entry[] = $rental->get_renewals();
     $table_body[] = $entry;
