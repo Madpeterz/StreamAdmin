d901f308959f791197a451eb559d1e4235ac72b5
Madpeter <madpeter@gmail.com>
2020-06-27 09:41:59 +0100

    gen code for R4 and R7

---
+++ public_html/gen_r7.php
@@ -0,0 +1,27 @@
+<?php
+/*
+    Please move this file up a level out of the gen folder before running ^+^
+*/
+// setup workarea
+define("framework_location","site/vendor/yetonemorephpframework/"); // db_objects and mysqli
+define("gen_database_host","localhost");
+define("gen_database_username","root");
+define("gen_database_password","");
+define("save_models_to_folder","site/model/");
+//define("add_db_to_table",true); // add the database name before the table name
+//define("source_databases",array("streamadmin","secondbothost"));
+
+define("add_db_to_table",false); // add the database name before the table name
+define("source_databases",array("streamadmin"));
+
+// load framework
+require_once("".framework_location."db_objects/loader.php"); // db_objects
+include("gen/create_db_class.php");
+require_once("".framework_location."mysqli/loader.php"); // sql_driver
+
+// connect to SQL
+$sql = new mysqli_controler();
+
+// lets rock
+include("gen/gen.php");
+?>
