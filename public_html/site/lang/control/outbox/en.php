a2811684f6c5cb44e59a169f61b1ab7c458772ee
Madpeter <madpeter@gmail.com>
2020-09-07 07:57:24 +0100

    outbox bulk send support

---
+++ public_html/site/lang/control/outbox/en.php
@@ -0,0 +1,9 @@
+<?php
+$lang = array(
+    "outbox.send.error.1" => "To many avatars sent vs what was expected",
+    "outbox.send.error.2" => "Unable to process source for bulk mail!",
+    "outbox.send.error.3" => "No rentals found with selected source/id pair",
+    "outbox.send.error.4" => "No avatars found to send to",
+    "outbox.send.ok" => "Sent to %1\$s avatars",
+);
+?>
