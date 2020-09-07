77450ebbbb98db31ec39b4fd2fc50e6b53b278f6
Madpeter <madpeter@gmail.com>
2020-09-07 07:16:29 +0100

    outbox bulk send support (views)

---
+++ public_html/site/view/outbox/bulk.server.php
@@ -0,0 +1,14 @@
+<?php
+$server_set = new server_set();
+$server_set->loadAll();
+
+
+$form = new form();
+$form->target("outbox/bulk/server");
+$form->mode("get");
+$form->col(4);
+    $form->select("serverlink","Server",0,$server_set->get_linked_array("id","domain"));
+$form->col(8);
+    $form->textarea("message","Message",800,"","Use swap tags as the placeholders! max length 800");
+$pages["Send => Bulk [Server]"] =  $form->render("Select avatars","primary")."<br/>Send mail to everyone with the selected server";
+?>
--- public_html/site/view/outbox/default.php
+++ public_html/site/view/outbox/default.php
@@ -1,33 +1,11 @@
 <?php
-$template_parts["page_title"] .= "Status";
-$services = array(
-    "notecard" => array("timeper"=>30,"classname"=>"notecard_set"),
-    "details" => array("timeper"=>15,"classname"=>"detail_set"),
-    "mail" => array("timeper"=>15,"classname"=>"message_set"),
-);
-$table_head = array("Outbox name","Pending","TTC");
-$table_body = array();
-foreach($services as $service_name => $config)
-{
-    $entry = array();
-    $entry[] = '<a href="[[url_base]]outbox/'.$service_name.'">'.$service_name.'</a>';
-    $object_set = new $config["classname"]();
-    $object_set->loadAll();
-    $entry[] = $object_set->get_count();
-    $time_to_clear = ($config["timeper"]*$object_set->get_count());
-    if($time_to_clear > 60)
-    {
-        $mins = floor($time_to_clear/60);
-        if($mins > 60)
-        {
-            $hours = floor($mins/60);
-            $entry[] = $mins." hours";
-        }
-        else $entry[] = $mins." mins";
-    }
-    else $entry[] = $time_to_clear." secs";
-    $table_body[] = $entry;
-}
-echo render_table($table_head,$table_body);
-echo "<br><hr/><p>TTC is the Expected time to clear<br/>this is if the SL service object is running normaly</p>";
+$pages = array();
+define("swaps_table_paged",true);
+include("site/view/outbox/status.php");
+include("site/view/outbox/bulk.package.php");
+include("site/view/outbox/bulk.server.php");
+include("site/view/outbox/bulk.status.php");
+include("site/view/shared/swaps_table.php");
+$paged_info = new paged_info();
+echo $paged_info->render($pages);
 ?>
