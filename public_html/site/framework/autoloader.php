371ae1f278d113d875cb851fcace166a624485ce
Madpeter <madpeter@gmail.com>
2020-09-10 13:54:49 +0100

    fixs to the helper auto loader

--- public_html/site/framework/autoloader.php
+++ public_html/site/framework/autoloader.php
@@ -12,16 +12,19 @@ function auto_load_model($class_name="")
 }
 function auto_load_api($class_name="")
 {
-	$loadfile = "site/serverapis/".$class_name.".php";
+	$loadfile = "site/api_server/".$class_name.".php";
 	if(file_exists($loadfile)) require_once($loadfile);
 }
 function auto_load_helper($class_name="")
 {
 	$bits = explode("_",$class_name);
-	array_pop($bits);
-	$try_class_file = implode("_",$bits);
-	$loadfile = "site/model_helpers/".$try_class_file.".helper.php";
-	if(file_exists($loadfile)) require_once($loadfile);
+	if(in_array("helper",$bits) == true)
+	{
+		array_pop($bits);
+		$class_name = implode("_",$bits);
+		$loadfile = "site/model_helpers/".$class_name.".helper.php";
+		if(file_exists($loadfile)) require_once($loadfile);
+	}
 }
 spl_autoload_register('auto_load_model');
 spl_autoload_register('auto_load_helper');
