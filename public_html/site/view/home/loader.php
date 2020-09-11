980696ed374627cf64d84541285928e6fb900d39
Madpeter <madpeter@gmail.com>
2020-09-11 18:39:04 +0100

    dashboard server load details skip when api is disabled

--- public_html/site/view/home/loader.php
+++ public_html/site/view/home/loader.php
@@ -84,12 +84,21 @@ foreach($server_set->get_all_ids() as $server_id)
 {
     $server = $server_set->get_object_by_id($server_id);
     $entry = array();
-    $entry[] = '<a href="[[url_base]]stream/onserver/'.$server->get_id().'"><strong>'.$server->get_domain().'</strong></a><br/><br/><div class="serverstatusdisplay">
-    <sub data-loading="
-    CPU: <span class=\'text-light\'>?</span> |
-    Ram: <span class=\'text-light\'>?</span> % |
-    Str: <span class=\'text-light\'>?</span> %"
-    data-repeatingrate="7000" class="ajaxonpageload" data-loadurl="[[url_base]]ajax.php/server/server_load/'.$server->get_id().'"></sub></div>';
+    $serverstatus = '<a href="[[url_base]]stream/onserver/'.$server->get_id().'"><strong>'.$server->get_domain().'</strong></a><br/><br/><div class="serverstatusdisplay">';
+    if($server->get_api_serverstatus() == true)
+    {
+        $serverstatus .= '<sub data-loading="
+        CPU: <span class=\'text-light\'>?</span> |
+        Ram: <span class=\'text-light\'>?</span> % |
+        Str: <span class=\'text-light\'>?</span> %"
+        data-repeatingrate="7000" class="ajaxonpageload" data-loadurl="[[url_base]]ajax.php/server/server_load/'.$server->get_id().'"></sub>';
+    }
+    else
+    {
+        $serverstatus .= '<sub> </sub>';
+    }
+    $serverstatus .= '</div>';
+    $entry[] = $serverstatus;
     $entry[] = $server_loads[$server->get_id()]["ready"];
     $entry[] = $server_loads[$server->get_id()]["needwork"];
     $entry[] = $server_loads[$server->get_id()]["sold"];
