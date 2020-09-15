ad13f6635ea8cf21344340d2f1cc7687bf25703c
Madpeter <madpeter@gmail.com>
2020-09-15 14:20:51 +0100

    adds why failed and event name to process event

--- public_html/site/lang/api_serverlogic/en.php
+++ public_html/site/lang/api_serverlogic/en.php
@@ -1,6 +1,6 @@
 <?php
 $api_logiclang = array(
-    "failed.create" => "Unable to create event",
+    "failed.create" => "Unable to create event %1\$s because: %2\$s",
     "failed.noapi" => "Unable to find API config",
     "failed.noserver" => "Unable to find server"
 );
