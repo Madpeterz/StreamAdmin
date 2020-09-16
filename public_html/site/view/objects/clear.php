9971dd5769507dbf345dde241d9966c63e8b90ea
Madpeter <madpeter@gmail.com>
2020-09-17 00:37:48 +0100

    Accept type box switched to button

--- public_html/site/view/objects/clear.php
+++ public_html/site/view/objects/clear.php
@@ -8,7 +8,16 @@ $form->target("objects/clear");
 $form->required(true);
 $form->col(6);
 $form->group("Clear all objects (DB only)");
-$form->text_input("accept","Type \"Accept\"",30,"","ok sure");
+$action = '
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-outline-danger active">
+    <input type="radio" value="Accept" name="accept" autocomplete="off" > Accept
+  </label>
+  <label class="btn btn-outline-secondary">
+    <input type="radio" value="Nevermind" name="accept" autocomplete="off" checked> Nevermind
+  </label>
+</div>';
+$form->direct_add($action);
 echo $form->render("Clear","warning");
 echo "";
 ?>
