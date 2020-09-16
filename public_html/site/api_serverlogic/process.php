58abb644c9bf963e2739067c1509a267a73d828d
Madpeter <madpeter@gmail.com>
2020-09-16 20:47:37 +0100

    trying to find the source of an error

--- public_html/site/api_serverlogic/process.php
+++ public_html/site/api_serverlogic/process.php
@@ -5,6 +5,11 @@ $api_logiclang = array(
     "failed.noserver" => "Unable to find server"
 );
 $api_serverlogic_reply = true;
+if(isset($site_lang) == false)
+{
+    error_log(print_r(debug_backtrace(), true));
+    $site_lang = "en";
+}
 $lang_file = "site/lang/api_serverlogic/".$site_lang.".php";
 if(file_exists($lang_file) == true) { include($lang_file); }
 if(isset($server) == false)
