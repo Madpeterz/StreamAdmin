546caae75924c0fd8c69c819b1df30ce14918b48
Madpeter <madpeter@gmail.com>
2020-10-03 07:12:03 +0100

    update theme files to use the new system (sidemenu first)

---
+++ public_html/site/theme/shared/ajax/template.php
@@ -0,0 +1,11 @@
+<?php
+$view_reply->load_template(
+    "sidemenu",
+    "streamadminr5",
+    array("topper","header","body_start","left_content","center_content","body_end","modals","footer")
+);
+$view_reply->set_swap_tag_string("html_menu","");
+$view_reply->set_swap_tag_string("page_title","");
+$view_reply->set_swap_tag_string("page_actions","");
+$view_reply->set_swap_tag_string("page_content","");
+?>
