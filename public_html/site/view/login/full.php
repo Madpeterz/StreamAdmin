c02ee96410ea68cd271e561338c4ba67e5e72eab
Madpeter <madpeter@gmail.com>
2020-10-14 16:16:20 +0100

    Update full.php

--- public_html/site/view/login/full.php
+++ public_html/site/view/login/full.php
@@ -1,24 +1,24 @@
 <?php
 if($area == "reset")
 {
-    $template_parts["html_title"] = "Reset password";
-    include("site/theme/".$site_theme."/blocks/login/reset.layout");
+    $view_reply->set_swap_tag_string("html_title","Reset password");
+    $view_reply->add_swap_tag_string("page_content",file_get_contents("site/theme/".$site_theme."/blocks/login/reset.layout"));
 }
 else if($area == "resetwithtoken")
 {
-    $template_parts["html_title"] = "Recover password";
-    include("site/theme/".$site_theme."/blocks/login/passwordrecover.layout");
+    $view_reply->set_swap_tag_string("html_title","Recover password");
+    $view_reply->add_swap_tag_string("page_content",file_get_contents("site/theme/".$site_theme."/blocks/login/passwordrecover.layout"));
 }
 else if($area == "logout")
 {
     $session->end_session();
-    redirect("");
+    $view_reply->redirect("");
 }
 else
 {
-    $template_parts["html_title"] = "Login";
-    $template_parts["why_logged_out"] = $session->get_why_logged_out();
-    include("site/theme/".$site_theme."/blocks/login/login.layout");
+    $view_reply->set_swap_tag_string("html_title","Login");
+    $view_reply->add_swap_tag_string("why_logged_out",$session->get_why_logged_out());
+    $view_reply->add_swap_tag_string("page_content",file_get_contents("site/theme/".$site_theme."/blocks/login/login.layout"));
 }
 
 ?>
