da3bf67ff72175e571c3b1d508174fea9aa02e5c
Madpeter <madpeter@gmail.com>
2020-10-20 17:17:44 +0100

    improve the view controler

--- public_html/site/view/view.php
+++ public_html/site/view/view.php
@@ -1,30 +1,26 @@
 <?php
-function get_require_path(string $module="",string $file="",bool $allow_downgrade=true) : ?string
+function load_module_view() : ?bool
 {
-    global $view_reply;
-    if($module == "login")
+    global $view_reply, $module;
+    $test_path = "site/view/".$module."/loader.php";
+    if(file_exists($test_path) == true)
     {
-        return "site/view/login/full.php";
+        include($test_path); // run normal loader
+        $check_file = "site/view/".$module."/".$area.".php";
+        if(file_exists($check_file) == true)
+        {
+            include($check_file); // run targeted area
+        }
+        return true;
     }
     else
     {
-        $test_path = "site/view/";
-        if($module != "") $test_path .= "/".$module;
-        if($file != "") $test_path .= "/".$file;
-        else $test_path .= "/loader.php";
-        if(file_exists($test_path) == true)
-        {
-            return $test_path;
-        }
-        else
-        {
-            $view_reply->set_swap_tag_string("html_title","Oh snap");
-            $view_reply->set_swap_tag_string("page_title","Oh snap");
-            $view_reply->set_swap_tag_string("page_actions","- ERROR -");
-            $view_reply->set_swap_tag_string("page_content","Unable to load ".$module." ".$file."");
-            return null;
-        }
+        $view_reply->set_swap_tag_string("html_title","Oh snap");
+        $view_reply->set_swap_tag_string("page_title","Oh snap");
+        $view_reply->set_swap_tag_string("page_actions","- ERROR -");
+        $view_reply->set_swap_tag_string("page_content","Unable to load ".$module." ".$file."");
     }
+    return false;
 }
 include("site/framework/loader_light.php");
 add_vendor("website");
@@ -37,16 +33,11 @@ if($module != "login")
     include("site/theme/streamadminr5/layout/sidemenu/template.php");
     if($area == "") $area = "default";
     require_once("site/view/shared/menu.php");
+    load_module_view();
 }
 else
 {
     include("site/theme/streamadminr5/layout/full/template.php");
-}
-$found_path = get_require_path($module,"",false);
-if($found_path != null)
-{
-    $view_reply->set_swap_tag_string("html_title",$module." / ");
-    require_once($found_path);
 }
 $view_reply->render_page();
 ?>
