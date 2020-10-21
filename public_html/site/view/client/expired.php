d50a7002b76c3385e75ec484f6d1271153ed0430
Madpeter <madpeter@gmail.com>
2020-10-21 01:24:06 +0100

    Update expired.php
    moves the page_actions to after the with_status code

--- public_html/site/view/client/expired.php
+++ public_html/site/view/client/expired.php
@@ -6,6 +6,6 @@ $whereconfig = array(
     "matches" => array("<="),
 );
 $view_reply->add_swap_tag_string("page_title","With status: Expired");
-$view_reply->set_swap_tag_string("page_actions","<a href='[[url_base]]client/bulkremove'><button type='button' class='btn btn-outline-danger btn-sm'>Bulk remove</button></a>");
 include("site/view/client/with_status.php");
+$view_reply->set_swap_tag_string("page_actions","<a href='[[url_base]]client/bulkremove'><button type='button' class='btn btn-outline-danger btn-sm'>Bulk remove</button></a>");
 ?>
