ca7c90f55e4b10919da3fe3e280a20bfdeca389b
Madpeter <madpeter@gmail.com>
2020-10-25 01:48:13 +0100

    bulk update switched from v1 to v2, and cleaned up

--- public_html/site/framework/db_objects/src/genClass_set/bulk.update.php
+++ public_html/site/framework/db_objects/src/genClass_set/bulk.update.php
@@ -1,97 +1,126 @@
 <?php
 abstract class genClass_collection_updatebulk extends genClass_collection_get
 {
-    public function update_single_field_for_collection($update_field="",$new_value="") : array
+    public function update_single_field_for_collection(string $update_field,?string $new_value) : array
     {
         return $this->update_multiple_fields_for_collection(array($update_field),array($new_value));
     }
-    public function update_multiple_fields_for_collection($update_fields=array(),$new_values=array()) : array
+    protected function update_multiple_make_update_config(array $update_fields,array $new_values) : array
+    {
+        $worker = new $this->worker_class();
+        $update_config = array(
+            "fields" => array(),
+            "values" => array(),
+            "types" => array(),
+        );
+        $all_ok = true;
+        $loop = 0;
+        while($loop < count($update_fields))
+        {
+            $lookup = "get_".$update_fields[$loop]."";
+            if(method_exists($worker,$lookup))
+            {
+                $field_type = $worker->get_field_type($update_fields[$loop],true);
+                if($field_type != null)
+                {
+                    $update_config["fields"][] = $update_fields[$loop];
+                    $update_config["values"][] = $new_values[$loop];
+                    $update_config["types"][] = $field_type;
+                }
+                else
+                {
+                    $all_ok = false;
+                    break;
+                }
+            }
+            else
+            {
+                $all_ok = false;
+                break;
+            }
+            $loop++;
+        }
+        return array("status"=>$all_ok,"dataset"=>$update_config);
+    }
+    protected function update_multiple_get_updated_ids(array $update_fields,array $new_values) : array
+    {
+        $expected_changes = 0;
+        $changed_ids = array();
+        foreach($this->get_all_ids() as $entry_id)
+        {
+            $localworker = $this->collected[$entry_id];
+            $loop2 = 0;
+            while($loop2 < count($update_fields))
+            {
+                $lookup = "get_".$update_fields[$loop2];
+                if($localworker->$lookup() != $new_values[$loop2])
+                {
+                    $expected_changes++;
+                    $changed_ids[] = $entry_id;
+                    break;
+                }
+                $loop2++;
+            }
+        }
+        return array("changes"=>$expected_changes,"changed_ids"=>$changed_ids);
+    }
+    protected function update_multiple_apply_changes(array $update_fields,array $new_values)
+    {
+        foreach($this->get_all_ids() as $entry_id)
+        {
+            $localworker = $this->collected[$entry_id];
+            $loop2 = 0;
+            while($loop2 < count($update_fields))
+            {
+                $applyer = "set_".$update_fields[$loop2];
+                $localworker->$applyer($new_values[$loop2]);
+                $loop2++;
+            }
+        }
+    }
+    public function update_multiple_fields_for_collection(array $update_fields,array $new_values) : array
     {
         if($this->get_count() > 0)
         {
-            if($this->worker_class != null)
+            if(count($update_fields) > 0)
             {
-                $worker = new $this->worker_class();
+                if($this->worker_class != null)
+                {
+                    $worker = new $this->worker_class();
 
-                $wherefields = array();
-                $wherevalues = array();
-                $setfields = array();
-                $setvalues = array();
-                $loop = 0;
-                $all_ok = true;
-                $lookup_failed_on = "";
-                while($loop < count($update_fields))
-                {
-                    $lookup = "get_".$update_fields[$loop]."";
-                    if(method_exists($worker,$lookup))
+                    $where_config = array(
+                        "fields"=>array("id"),
+                        "matches"=>array("IN"),
+                        "values"=>array(),
+                        "types"=>array("i"),
+                    );
+                    $ready_update_config = $this->update_multiple_make_update_config($update_fields,$new_values);
+                    if($ready_update_config["status"] == true)
                     {
-                        $field_type = $worker->get_field_type($update_fields[$loop],true);
-                        if($field_type != null)
+                        $update_config = $ready_update_config["dataset"];
+                        unset($ready_update_config);
+                        $change_config = $this->update_multiple_get_updated_ids($update_fields,$new_values);
+                        if($change_config["changes"] > 0)
                         {
-                            $setfields[] = $update_fields[$loop];
-                            $setvalues[] = array($new_values[$loop] => $field_type);
-                        }
-                        else
-                        {
-                            $lookup_failed_on = "Unable to find fieldtype for: ".$update_fields[$loop]."";
-                            $all_ok = false;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        $lookup_failed_on = "Unable to find method: ".$lookup."";
-                        $all_ok = false;
-                        break;
-                    }
-                    $loop++;
-                }
-                if($all_ok == true)
-                {
-                    $expected_changes = 0;
-                    foreach($this->get_all_ids() as $entry_id)
-                    {
-                        $localworker = $this->collected[$entry_id];
-                        $loop2 = 0;
-                        while($loop2 < count($update_fields))
-                        {
-                            $lookup = "get_".$update_fields[$loop2];
-                            if($localworker->$lookup() != $new_values[$loop2])
+                            $where_config["values"][] = $change_config["changed_ids"];
+                            $update_status = $this->sql->updateV2($worker->get_table(),$update_config,$where_config,$change_config["changes"]);
+                            unset($change_config);
+                            if($update_status["status"] == true)
                             {
-                                $expected_changes++;
-                                $wherefields[] = array("id" => "=");
-                                $wherevalues[] = array($entry_id => "i");
-                                break;
+                                $this->update_multiple_apply_changes($update_fields,$new_values);
+                                return $update_status;
                             }
-                            $loop2++;
+                            return array("status"=>false,"changes"=>0,"message"=>"Update failed because:".$update_status["message"]);
                         }
+                        return array("status"=>true,"changes"=>0,"message"=>"No changes made to collection");
                     }
-                    if($expected_changes > 0)
-                    {
-                        $update_status = $this->sql->update($worker->get_table(), $setfields, $setvalues, $wherefields, $wherevalues, "OR",$expected_changes);
-                        if($update_status["status"] == true)
-                        {
-                            foreach($this->get_all_ids() as $entry_id)
-                            {
-                                $localworker = $this->collected[$entry_id];
-                                $loop2 = 0;
-                                while($loop2 < count($update_fields))
-                                {
-                                    $setter = "set_".$update_fields[$loop2];
-                                    $localworker->$setter($new_values[$loop2]);
-                                    $loop2++;
-                                }
-                            }
-                        }
-                        else return $update_status;
-                    }
-                    else return array("status"=>true,"changes"=>0,"message"=>"no changes to be made");
+                    return array("status"=>false,"changes"=>0,"message"=>"Unable to find all field types needed for update!");
                 }
-                else return array("status"=>false,"changes"=>0,"message"=>$lookup_failed_on);
+                return array("status"=>false,"changes"=>0,"message"=>"worker not setup");
             }
-            else return array("status"=>false,"changes"=>0,"message"=>"worker not setup");
+            return array("status"=>false,"changes"=>0,"message"=>"No fields being updated!");
         }
-        else return array("status"=>false,"changes"=>0,"message"=>"Nothing loaded in collection");
+        return array("status"=>false,"changes"=>0,"message"=>"Nothing loaded in collection");
     }
 }
 ?>
