0709ad60e3372ce09720f1aea1b8aa66fb50f46b
Madpeter <madpeter@gmail.com>
2020-10-27 23:55:24 +0000

    improve dashboard objects display to be SL health

--- streamadmin/webpanel/view/home/dashboard/loaders/objects.php
+++ streamadmin/webpanel/view/home/dashboard/loaders/objects.php
@@ -1,5 +1,21 @@
 <?php
 $objects = new objects();
+$owner_objects_list = array(
+    "apirequests",
+    "mailserver",
+    "noticeserver",
+    "detailsserver",
+    "notecardsserver"
+);
 $one_hour_ago = (time()-$unixtime_hour);
-$countdata_objects = $sql->group_count($objects->get_table(),"objectmode",array(array("lastseen"=>">=")),array(array($one_hour_ago=>"i")));
+$objects_set = new objects_set();
+$where_config = array(
+     "fields"=>array("avatarlink","lastseen","objectmode"),
+     "matches"=>array("=",">=","IN"),
+     "values"=>array($slconfig->get_owner_av(),$one_hour_ago,$owner_objects_list),
+     "types"=>array("i","i","s"),
+);
+$objects_set->load_with_config($where_config);
+$region_set = new region_set();
+$region_set->load_ids($objects_set->get_all_by_field("regionlink"));
 ?>
