6a9414acbfe9986b3200faef909eecf24bb5c602
Madpeter <madpeter@gmail.com>
2020-10-28 00:13:40 +0000

    adjust the way composer loads

--- streamadmin/webpanel/vendor/composer/autoload_real.php
+++ streamadmin/webpanel/vendor/composer/autoload_real.php
@@ -34,22 +34,13 @@ class ComposerAutoloaderInit32c7e90c469273af0a779f4886396be4
 
             call_user_func(\Composer\Autoload\ComposerStaticInit32c7e90c469273af0a779f4886396be4::getInitializer($loader));
         } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
+        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         if ($useStaticLoader) {
