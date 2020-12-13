bb6e1cf93871e7cd9c792e831efa5af87a7ccdf6
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2020-12-13 08:06:43 +0000

    new index file super clean

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -2,15 +2,8 @@
 
 namespace App;
 
+use App\Framework\Switchboard;
+
 include "../App/Framework/load.php";
 
-$loadwith = "App\View\Switchboard";
-if (install_ok() == true) {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $loadwith = "Control";
-    }
-} else {
-    $method = "Install";
-}
-
-$obj = new $loadwith();
+new Switchboard();
