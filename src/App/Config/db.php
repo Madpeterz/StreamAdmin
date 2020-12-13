54123750d09ee9ba74376100700d2b9a669136b0
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2020-12-13 08:05:29 +0000

    Update db.php

--- src/App/Config/db.php
+++ src/App/Config/db.php
@@ -2,8 +2,12 @@
 
 if (getenv('DB_HOST') !== false) {
     include "../App/Config/default.db.php";
+    if (getenv('DB_HOST') !== false) {
+        include "../App/Flags/DbConfigFound.php";
+    }
 } else {
     if (file_exists("../App/Config/db_installed.php") == true) {
+        include "../App/Flags/DbConfigFound.php";
         include "../App/Config/db_installed.php";
     }
 }
