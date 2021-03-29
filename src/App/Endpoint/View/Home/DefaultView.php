397f0968c55e2819aed0f3711f168fe300270528
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-03-29 13:20:10 +0100

    Update DefaultView.php

--- src/App/Endpoint/View/Home/DefaultView.php
+++ src/App/Endpoint/View/Home/DefaultView.php
@@ -2,6 +2,7 @@
 
 namespace App\Endpoint\View\Home;
 
+use App\Template\Form;
 use App\Template\Grid;
 
 class DefaultView extends HomeDisplayData
@@ -11,7 +12,41 @@ class DefaultView extends HomeDisplayData
         $this->main_grid = new Grid();
         $this->loadDatasets();
         $this->displayDatasets();
-
+        if ($this->session->getOwnerLevel() == 1) {
+            $this->unsafeWorkspace();
+        }
         $this->output->addSwapTagString("page_content", $this->main_grid->getOutput());
+    }
+
+    protected function unsafeWorkspace(): void
+    {
+        $need_cleanup = false;
+        $why_unsafe = "";
+        if (is_dir(ROOTFOLDER . '/App/public_html/fake') == true) {
+            $need_cleanup = true;
+            $why_unsafe = "faker public folder found";
+        }
+        if (is_dir(ROOTFOLDER . 'tests') == true) {
+            $need_cleanup = true;
+            if ($why_unsafe != "") {
+                $why_unsafe .= " , ";
+            }
+            $why_unsafe .= " tests folder found ";
+        }
+        if ($need_cleanup == true) {
+            $form = new Form();
+            $form->mode("post");
+            $form->target("home/cleanup");
+            $formcode = $form->render("Cleanup", "Danger");
+            $this->main_grid->addContent('<div class="jumbotron">
+            <h1 class="display-4">Secure install</h1>
+            <p class="lead">' . $why_unsafe . '</p>
+            <hr class="my-4">
+            <p>Please run the cleanup tool now!</p>
+            <p class="lead">
+                ' . $formcode . '
+            </p>
+          </div>', 12);
+        }
     }
 }
