2c779cd83d36f27ffcc13317cc3aae9afa35cdec
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-04-10 23:14:52 +0100

    Update RestApi.php
    changes to error log

--- src/App/MediaServer/Abstracts/RestApi.php
+++ src/App/MediaServer/Abstracts/RestApi.php
@@ -76,7 +76,7 @@ abstract class RestApi extends ErrorLogging
                 $body = $this->getPostFormated($postdata);
             }
             $res = $this->client->request($method, $endpoint, $body);
-            error_log(json_encode(["body" => $body, "method" => $method, "endpoint" => $endpoint]));
+            error_log($body);
             if ($res->getStatusCode() == 200) {
                 return ["status" => true,"message" => $res->getBody()->getContents()];
             } else {
