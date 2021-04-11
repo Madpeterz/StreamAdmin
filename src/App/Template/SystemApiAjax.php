2aa0310062b68fe7977afa26ca7cf2572d5c5cba
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-04-11 15:11:04 +0100

    Update SystemApiAjax.php

--- src/App/Template/SystemApiAjax.php
+++ src/App/Template/SystemApiAjax.php
@@ -25,7 +25,6 @@ abstract class SystemApiAjax extends ViewAjax
         parent::__construct($AutoLoadTemplate);
         $this->requiredValues();
         $this->timeWindow();
-        $this->hashCheck();
         if ($this->load_ok == false) {
             $this->setSwapTag("status", false);
             return;
@@ -62,48 +61,14 @@ abstract class SystemApiAjax extends ViewAjax
         if ($this->load_ok == false) {
             return;
         }
-        $required_sl = [
-            "method",
-            "action",
-            "mode",
-        ];
-
         $input = new InputFilter();
-        $this->staticpart = "";
-        foreach ($required_sl as $slvalue) {
-            $value = $input->postFilter($slvalue);
-            if ($value !== null) {
-                $this->$slvalue = $value;
-                $this->staticpart .= $value;
-            } else {
-                $this->load_ok = false;
-                $this->setSwapTag("message", "Value: " . $slvalue . " is missing");
-                break;
-            }
-        }
         $this->unixtime = $input->postFilter("unixtime");
-        if ($this->unixtime === null) {
-            $this->load_ok = false;
-        }
-        $this->hash = $input->postFilter("hash");
-        if ($this->hash === null) {
+        $this->token = $input->postFilter("token");
+        if ($this->token == $this->slconfig->getHttpInboundSecret()) {
             $this->load_ok = false;
         }
         if ($this->load_ok == false) {
             $this->setSwapTag("message", "One or more required values are missing");
-            return;
-        }
-    }
-    protected function hashCheck(): void
-    {
-        if ($this->load_ok == false) {
-            return;
-        }
-        $raw = $this->unixtime . "" . $this->staticpart . "" . $this->slconfig->getHttpInboundSecret();
-        $hashcheck = sha1($raw);
-        if ($hashcheck != $this->hash) {
-            $this->load_ok = false;
-            $this->setSwapTag("message", "Unable to vaildate request to API endpoint: ");
             return;
         }
     }
