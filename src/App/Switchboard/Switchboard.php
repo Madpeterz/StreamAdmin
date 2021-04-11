e7fd07ff837a0e348da98578c672df51bbca9f3d
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-04-11 14:16:58 +0100

    updates switchboards

--- src/App/Switchboard/Switchboard.php
+++ src/App/Switchboard/Switchboard.php
@@ -19,27 +19,48 @@ abstract class Switchboard
     protected SessionControl $session;
     public function __construct()
     {
-        global $module, $area;
-        $this->module = $module;
-        $this->area = $area;
         $this->loadPage();
+    }
+
+    protected function accessChecks(): bool
+    {
+        if ((install_ok() == false) && ($this->method != "Install")) {
+            print json_encode(["status" => "0", "message" => "Error with setup please contact support"]);
+            return false;
+        }
+        return true;
     }
 
     protected function loadPage(): void
     {
+        global $module, $area;
+        $this->module = $module;
+        $this->area = $area;
         $input = new InputFilter();
         if ($this->method == "") {
             $this->method = $input->postFilter("method");
+            $this->module = $this->method;
         }
         if ($this->action == "") {
             $this->action = $input->postFilter("action");
+            $this->area = $this->action;
+        }
+        if ($this->method == "") {
+            $this->method = $this->module;
+        }
+        if ($this->action == "") {
+            $this->action = $this->area;
+        }
+        if ($this->method == "") {
+            $this->method = "Home";
+            $this->module = "Home";
+        }
+        if ($this->accessChecks() == false) {
+            return;
         }
         $this->method = ucfirst($this->method);
         $this->action = ucfirst($this->action);
-        if ((install_ok() == false) && ($this->method != "Install")) {
-            print json_encode(["status" => "0", "message" => "Error with setup please contact support"]);
-            return;
-        }
+
         $use_class = "\\App\\Endpoint\\" . $this->targetEndpoint . "\\" . $this->method . "";
         if ($this->method == "") {
             print json_encode(["status" => "0", "message" => "No control method selected!"]);
--- src/App/Switchboard/Website.php
+++ src/App/Switchboard/Website.php
@@ -4,32 +4,36 @@ namespace App\Switchboard;
 
 class Website extends Switchboard
 {
+    protected function accessChecks(): bool
+    {
+        global $_SERVER;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->targetEndpoint = "Control";
+        }
+        if (install_ok() == false) {
+            $this->method = "Install";
+            $this->module = "Install";
+            include "" . ROOTFOLDER . "/App/Flags/InstallMode.php";
+            return true;
+        }
+        if ($this->session->getLoggedIn() == true) {
+            return true;
+        }
+        $this->method = "Login";
+        $this->module = "Login";
+        $allowed_login_areas = ["DefaultView","Logout","Reset","Resetwithtoken","Resetnow","Start"];
+        if (in_array($this->action, $allowed_login_areas) == false) {
+            $this->action = "DefaultView";
+            $this->area = "DefaultView";
+        }
+        return true;
+    }
     public function __construct()
     {
-        global $page, $session, $_SERVER;
+        global $page, $session;
         $this->page = $page;
         $this->session = &$session;
         $this->targetEndpoint = "View";
-        $this->module = ucfirst($this->module);
-        $this->area = ucfirst($this->area);
-        if (install_ok() == true) {
-            if ($this->session->getLoggedIn() == false) {
-                $this->module = "Login";
-                $allowed_login_areas = ["DefaultView","Logout","Reset","Resetwithtoken","Resetnow","Start"];
-                if (in_array($this->area, $allowed_login_areas) == false) {
-                    $this->area = "DefaultView";
-                }
-            }
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $this->targetEndpoint = "Control";
-            }
-        } else {
-            $this->module = "Install";
-            include "" . ROOTFOLDER . "/App/Flags/InstallMode.php";
-        }
-        if ($this->module == "") {
-            $this->module = "Home";
-        }
         $this->loadPage();
     }
 }
