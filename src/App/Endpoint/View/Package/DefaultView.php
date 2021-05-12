b159c7670e338124d630f32ae8ca9ccd1c113721
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-12 09:45:17 +0100

    Update DefaultView.php
    adds AutoDJ display to packages list table

--- src/App/Endpoint/View/Package/DefaultView.php
+++ src/App/Endpoint/View/Package/DefaultView.php
@@ -12,7 +12,7 @@ class DefaultView extends View
         $package_set = new PackageSet();
         $package_set->loadAll();
 
-        $table_head = ["id","UID","Name","Listeners","Days","Kbps","Cost"];
+        $table_head = ["id","UID","Name","Listeners","Days","Kbps","Cost","AutoDJ [GB]"];
         $table_body = [];
 
         foreach ($package_set->getAllIds() as $package_id) {
@@ -26,6 +26,11 @@ class DefaultView extends View
             $entry[] = $package->getDays();
             $entry[] = $package->getBitrate();
             $entry[] = $package->getCost();
+            $autoDJ = "No";
+            if ($package->getAutodj() == true) {
+                $autoDJ = $package->getAutodjSize();
+            }
+            $entry[] = $autoDJ;
             $table_body[] = $entry;
         }
         $this->setSwapTag("page_content", $this->renderDatatable($table_head, $table_body));
