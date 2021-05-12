3788fc7b56267a3e4a3c7057ab116ce6c1076fa9
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-12 11:09:20 +0100

    Update Switchboard.php
    improve notSet check and imrpove switchboard code

--- src/App/Switchboard/Switchboard.php
+++ src/App/Switchboard/Switchboard.php
@@ -9,16 +9,15 @@ use YAPF\InputFilter\InputFilter;
 abstract class Switchboard
 {
     protected $module = "";
-    protected $page = "";
     protected $area = "";
-    protected $option = "";
 
-    protected $method = "";
-    protected $action = "";
     protected $targetEndpoint = "";
     protected SessionControl $session;
     public function __construct()
     {
+        global $module, $area;
+        $this->module = $module;
+        $this->area = $area;
         $this->loadPage();
     }
 
@@ -33,7 +32,7 @@ abstract class Switchboard
 
     protected function notSet(?string $input): bool
     {
-        if (($input == "") || ($input == null)) {
+        if (($input === "") || ($input === null)) {
             return true;
         }
         return false;
@@ -41,50 +40,43 @@ abstract class Switchboard
 
     protected function loadPage(): void
     {
-        global $module, $area;
-        $this->module = $module;
-        $this->area = $area;
         $input = new InputFilter();
-        if ($this->notSet($this->method) == true) {
-            $this->method = $input->postFilter("method");
+        $usedPostSources = "No";
+
+        $postLoad = $input->postFilter("method");
+        if ($this->notSet($postLoad) == false) {
+            $this->module = $postLoad;
+            $usedPostSources = "Yes";
         }
-        if ($this->notSet($this->action) == true) {
-            $this->action = $input->postFilter("action");
+        $postLoad = $input->postFilter("action");
+        if ($this->notSet($postLoad) == false) {
+            $this->area = $postLoad;
+            $usedPostSources = "Yes";
         }
-        if ($this->notSet($this->method) == true) {
-            $this->method = $this->module;
+        $this->module = ucfirst($this->module);
+        $this->area = ucfirst($this->area);
+        if ($this->notSet($this->module) == true) {
+            $this->module = "Home";
         }
-        if ($this->notSet($this->action) == true) {
-            $this->action = $this->area;
-        }
-        if ($this->notSet($this->method) == true) {
-            $this->method = "Home";
-        }
-        $this->method = ucfirst($this->method);
-        $this->action = ucfirst($this->action);
-        $this->module = $this->method;
-        $this->area = $this->action;
         if ($this->accessChecks() == false) {
             return;
         }
-        $this->method = ucfirst($this->method);
-        $this->action = ucfirst($this->action);
-
-        $use_class = "\\App\\Endpoint\\" . $this->targetEndpoint . "\\" . $this->method . "";
-        if ($this->method == "") {
-            print json_encode(["status" => "0", "message" => "No control method selected!"]);
+        $use_class = "\\App\\Endpoint\\" . $this->targetEndpoint . "\\" . $this->module . "";
+        if ($this->module == "") {
+            print json_encode(["status" => "0", "message" => "No control module selected!"]);
             return;
         }
-        if ($this->action != "") {
-            $use_class .= "\\" . $this->action;
+        if ($this->area != "") {
+            $use_class .= "\\" . $this->area;
         } else {
+            $this->area = "DefaultView";
             $use_class .= "\\DefaultView";
         }
         if (class_exists($use_class) == false) {
             $old_use_class = $use_class;
             $use_class = "\\App\\Endpoint\\" . $this->targetEndpoint . "\\DefaultView";
             if (class_exists($use_class) == false) {
-                print json_encode(["status" => "0", "message" => "[" . $this->method . " | " . $this->action . "] 
+                print json_encode(["status" => "0", "message" => "[" . $this->module . " | " . $this->area . "] 
                 Unsupported class " . htmlentities($old_use_class)]);
                 error_log("Warning unable to load: " . $old_use_class . " and DefaultView is not supported");
                 return;
@@ -95,8 +87,9 @@ abstract class Switchboard
 
         $obj = new $use_class();
         if ($obj->getLoadOk() == true) {
-            $obj->getOutputObject()->setSwapTag("method", $this->method);
-            $obj->getOutputObject()->setSwapTag("action", $this->action);
+            $obj->getOutputObject()->setSwapTag("module", $this->module);
+            $obj->getOutputObject()->setSwapTag("area", $this->area);
+            $obj->getOutputObject()->setSwapTag("UsedPostSources", $usedPostSources);
             $obj->process();
         }
         $obj->getoutput();
