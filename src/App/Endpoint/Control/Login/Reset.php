01c370a8ee01a353dfd1b8aab6f373e56bccc082
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-12 10:31:09 +0100

    Update Reset.php
    stops leaking testable data via the reset function

--- src/App/Endpoint/Control/Login/Reset.php
+++ src/App/Endpoint/Control/Login/Reset.php
@@ -55,7 +55,7 @@ class Reset extends ViewAjax
                 }
             }
         }
-        $this->setSwapTag("status", $status);
+        $this->setSwapTag("status", true);
         $this->setSwapTag("redirect", "here");
         $this->setSwapTag("message", "If the account was found the reset code is on the way.");
     }
