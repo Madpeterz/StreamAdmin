4cc535071993edc542ef1e9b65fee6729753e963
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-15 13:13:10 +0100

    Update db.php

--- src/App/Config/db.php
+++ src/App/Config/db.php
@@ -2,9 +2,7 @@
 
 if (getenv('DB_HOST') !== false) {
     include ROOTFOLDER . "/App/Config/default.db.php";
-    if (getenv('DB_HOST') !== false) {
-        include ROOTFOLDER . "/App/Flags/DbConfigFound.php";
-    }
+    include ROOTFOLDER . "/App/Flags/DbConfigFound.php";
 } else {
     if (file_exists(ROOTFOLDER . "/App/Config/db_installed.php") == true) {
         include ROOTFOLDER . "/App/Flags/DbConfigFound.php";
