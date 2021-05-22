c130ce00f4e6c5e8ad3dcc40e67b5dcddf0c161b
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-22 21:14:45 +0100

    Update DefaultView.php
    adds Type to the package list

--- src/App/Endpoint/View/Package/DefaultView.php
+++ src/App/Endpoint/View/Package/DefaultView.php
@@ -3,6 +3,7 @@
 namespace App\Endpoint\View\Package;
 
 use App\R7\Set\PackageSet;
+use App\R7\Set\ServertypesSet;
 
 class DefaultView extends View
 {
@@ -11,17 +12,21 @@ class DefaultView extends View
         $this->output->addSwapTagString("page_title", " All");
         $package_set = new PackageSet();
         $package_set->loadAll();
+        $servertypes_set = new ServertypesSet();
+        $servertypes_set->loadAll();
 
-        $table_head = ["id","UID","Name","Listeners","Days","Kbps","Cost","AutoDJ [GB]"];
+        $table_head = ["id","UID","Name","Type","Listeners","Days","Kbps","Cost","AutoDJ [GB]"];
         $table_body = [];
 
         foreach ($package_set->getAllIds() as $package_id) {
             $package = $package_set->getObjectByID($package_id);
+            $type = $servertypes_set->getObjectByID($package->getServertypeLink());
             $entry = [];
             $entry[] = $package->getId();
             $entry[] = '<a href="[[url_base]]package/manage/' . $package->getPackageUid() . '">'
              . $package->getPackageUid() . '</a>';
             $entry[] = $package->getName();
+            $entry[] = $type->getName();
             $entry[] = $package->getListeners();
             $entry[] = $package->getDays();
             $entry[] = $package->getBitrate();
