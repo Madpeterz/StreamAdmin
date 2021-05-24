c4853a693ef812e6317cb1f70dd71b21b628598b
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-24 15:50:09 +0100

    Update Update.php
    improve code layout

--- src/App/Endpoint/Control/Package/Update.php
+++ src/App/Endpoint/Control/Package/Update.php
@@ -10,120 +10,157 @@ use YAPF\InputFilter\InputFilter;
 
 class Update extends ViewAjax
 {
+    protected Template $template;
+    protected Servertypes $servertype;
+    protected InputFilter $input;
+    protected Package $package;
+
+
+    protected ?string $name;
+    protected ?int $templateLink;
+    protected ?int $cost;
+    protected ?int $days;
+    protected ?int $bitrate;
+    protected ?int $listeners;
+    protected ?string $textureSoldout;
+    protected ?string $textureInstockSmall;
+    protected ?string $textureInstockSelected;
+    protected ?bool $autodj;
+    protected ?int $autodjSize;
+    protected ?string $apiTemplate;
+    protected ?int $servertypeLink;
+
     public function process(): void
     {
-        $template = new Template();
-        $servertype = new Servertypes();
-        $input = new InputFilter();
-        $package = new Package();
-
-        $name = $input->postFilter("name");
-        $templateLink = $input->postFilter("templateLink", "integer");
-        $cost = $input->postFilter("cost", "integer");
-        $days = $input->postFilter("days", "integer");
-        $bitrate = $input->postFilter("bitrate", "integer");
-        $listeners = $input->postFilter("listeners", "integer");
-        $textureSoldout = $input->postFilter("textureSoldout", "uuid");
-        $textureInstockSmall = $input->postFilter("textureInstockSmall", "uuid");
-        $textureInstockSelected = $input->postFilter("textureInstockSelected", "uuid");
-        $autodj = $input->postFilter("autodj", "bool");
-        $autodjSize = $input->postFilter("autodjSize", "integer");
-        $apiTemplate = $input->postFilter("apiTemplate");
-        $servertypeLink = $input->postFilter("servertypeLink", "integer");
-
-        if (strlen($name) < 5) {
-            $this->setSwapTag("message", "Name length must be 5 or longer");
+        $this->setup();
+        $this->formData();
+        if ($this->tests() == false) {
+            return;
+        } elseif ($this->loadData() == false) {
+            return;
+        } elseif ($this->savePackage() == false) {
             return;
         }
-        if (strlen($name) > 60) {
-            $this->setSwapTag("message", "Name must be 30 or less");
-            return;
-        }
-        if ($cost < 1) {
-            $this->setSwapTag("message", "Cost must be 1 or more");
-            return;
-        }
-        if ($cost > 99999) {
-            $this->setSwapTag("message", "Cost must be 99999 or less");
-            return;
-        }
-        if ($days < 1) {
-            $this->setSwapTag("message", "Days must be 1 or more");
-            return;
-        }
-        if ($days > 999) {
-            $this->setSwapTag("message", "Days must be 999 or less");
-            return;
-        }
-        if ($bitrate < 56) {
-            $this->setSwapTag("message", "bitrate must be 56 or more");
-            return;
-        }
-        if ($bitrate > 999) {
-            $this->setSwapTag("message", "bitrate must be 999 or less");
-            return;
-        }
-        if ($listeners < 1) {
-            $this->setSwapTag("message", "listeners must be 1 or more");
-            return;
-        }
-        if ($listeners > 999) {
-            $this->setSwapTag("message", "listeners must be 999 or less");
-            return;
-        }
-        if (strlen($textureSoldout) != 36) {
-            $this->setSwapTag("message", "Texture sold out must be a uuid");
-            return;
-        }
-        if (strlen($textureInstockSmall) != 36) {
-            $this->setSwapTag("message", "Texture instock small out must be a uuid");
-            return;
-        }
-        if (strlen($textureInstockSelected) != 36) {
-            $this->setSwapTag("message", "Texture instock selected out must be a uuid");
-            return;
-        }
-        if ($autodjSize > 9999) {
-            $this->setSwapTag("message", "AutoDJ size must be 9999 or less");
-            return;
-        }
-        if ($template->loadID($templateLink) == false) {
-            $this->setSwapTag("message", "Unable to find template");
-            return;
-        }
-        if (strlen($apiTemplate) > 50) {
-            $this->setSwapTag("message", "API template name can not be longer than 50");
-            return;
-        }
-        if (strlen($apiTemplate) < 3) {
-            $this->setSwapTag("message", "API template name can not be shorter than 3");
-            return;
-        }
-        if ($servertype->loadID($servertypeLink) == false) {
-            $this->setSwapTag("message", "Unable to find server type");
-            return;
-        }
-
         $this->setSwapTag("redirect", "package");
-        if ($package->loadByField("packageUid", $this->page) == false) {
-            $this->setSwapTag("message", "Unable to load package");
-            return;
-        }
-        $package->setName($name);
-        $package->setAutodj($autodj);
-        $package->setAutodjSize($autodjSize);
-        $package->setListeners($listeners);
-        $package->setBitrate($bitrate);
-        $package->setTemplateLink($templateLink);
-        $package->setCost($cost);
-        $package->setDays($days);
-        $package->setTextureSoldout($textureSoldout);
-        $package->setTextureInstockSmall($textureInstockSmall);
-        $package->setTextureInstockSelected($textureInstockSelected);
-        $package->setApiTemplate($apiTemplate);
-        $package->setServertypeLink($servertypeLink);
+        $this->setSwapTag("status", true);
+        $this->setSwapTag("message", "Package updated");
+    }
 
-        $update_status = $package->updateEntry();
+    protected function setup(): void
+    {
+        $this->template = new Template();
+        $this->servertype = new Servertypes();
+        $this->input = new InputFilter();
+        $this->package = new Package();
+    }
+
+    protected function formData(): void
+    {
+        $this->name = $this->input->postFilter("name");
+        $this->templateLink = $this->input->postFilter("templateLink", "integer");
+        $this->cost = $this->input->postFilter("cost", "integer");
+        $this->days = $this->input->postFilter("days", "integer");
+        $this->bitrate = $this->input->postFilter("bitrate", "integer");
+        $this->listeners = $this->input->postFilter("listeners", "integer");
+        $this->textureSoldout = $this->input->postFilter("textureSoldout", "uuid");
+        $this->textureInstockSmall = $this->input->postFilter("textureInstockSmall", "uuid");
+        $this->textureInstockSelected = $this->input->postFilter("textureInstockSelected", "uuid");
+        $this->autodj = $this->input->postFilter("autodj", "bool");
+        $this->autodjSize = $this->input->postFilter("autodjSize", "integer");
+        $this->apiTemplate = $this->input->postFilter("apiTemplate");
+        $this->servertypeLink = $this->input->postFilter("servertypeLink", "integer");
+    }
+
+    protected function tests(): bool
+    {
+        if (strlen($this->name) < 5) {
+            $this->setSwapTag("message", "Name length must be 5 or longer");
+            return false;
+        } elseif (strlen($this->name) > 60) {
+            $this->setSwapTag("message", "Name must be 30 or less");
+            return false;
+        } elseif ($this->cost < 1) {
+            $this->setSwapTag("message", "Cost must be 1 or more");
+            return false;
+        } elseif ($this->cost > 99999) {
+            $this->setSwapTag("message", "Cost must be 99999 or less");
+            return false;
+        } elseif ($this->days < 1) {
+            $this->setSwapTag("message", "Days must be 1 or more");
+            return false;
+        } elseif ($this->days > 999) {
+            $this->setSwapTag("message", "Days must be 999 or less");
+            return false;
+        } elseif ($this->bitrate < 56) {
+            $this->setSwapTag("message", "bitrate must be 56 or more");
+            return false;
+        } elseif ($this->bitrate > 999) {
+            $this->setSwapTag("message", "bitrate must be 999 or less");
+            return false;
+        } elseif ($this->listeners < 1) {
+            $this->setSwapTag("message", "listeners must be 1 or more");
+            return false;
+        } elseif ($this->listeners > 999) {
+            $this->setSwapTag("message", "listeners must be 999 or less");
+            return false;
+        } elseif (strlen($this->textureSoldout) != 36) {
+            $this->setSwapTag("message", "Texture sold out must be a uuid");
+            return false;
+        } elseif (strlen($this->textureInstockSmall) != 36) {
+            $this->setSwapTag("message", "Texture instock small out must be a uuid");
+            return false;
+        } elseif (strlen($this->textureInstockSelected) != 36) {
+            $this->setSwapTag("message", "Texture instock selected out must be a uuid");
+            return false;
+        } elseif ($this->autodjSize > 9999) {
+            $this->setSwapTag("message", "AutoDJ size must be 9999 or less");
+            return false;
+        } elseif (strlen($this->apiTemplate) > 50) {
+            $this->setSwapTag("message", "API template name can not be longer than 50");
+            return false;
+        } elseif (strlen($this->apiTemplate) < 3) {
+            $this->setSwapTag("message", "API template name can not be shorter than 3");
+            return false;
+        }
+        return true;
+    }
+
+    protected function loadData(): bool
+    {
+        if ($this->template->loadID($this->templateLink) == false) {
+            $this->setSwapTag("message", "Unable to find template");
+            return false;
+        } elseif ($this->servertype->loadID($this->servertypeLink) == false) {
+            $this->setSwapTag("message", "Unable to find server type");
+            return false;
+        } elseif ($this->package->loadByField("packageUid", $this->page) == false) {
+            $this->setSwapTag("message", "Unable to load package");
+            return false;
+        }
+        return true;
+    }
+
+    protected function updatePackageSettings(): void
+    {
+        $this->package->setName($this->name);
+        $this->package->setAutodj($this->autodj);
+        $this->package->setAutodjSize($this->autodjSize);
+        $this->package->setListeners($this->listeners);
+        $this->package->setBitrate($this->bitrate);
+        $this->package->setTemplateLink($this->templateLink);
+        $this->package->setCost($this->cost);
+        $this->package->setDays($this->days);
+        $this->package->setTextureSoldout($this->textureSoldout);
+        $this->package->setTextureInstockSmall($this->textureInstockSmall);
+        $this->package->setTextureInstockSelected($this->textureInstockSelected);
+        $this->package->setApiTemplate($this->apiTemplate);
+        $this->package->setServertypeLink($this->servertypeLink);
+    }
+
+    protected function savePackage(): bool
+    {
+        $this->updatePackageSettings();
+        $update_status = $this->package->updateEntry();
         if ($update_status["status"] == false) {
             $this->setSwapTag(
                 "message",
@@ -132,9 +169,8 @@ class Update extends ViewAjax
                     $update_status["message"]
                 )
             );
-            return;
+            return false;
         }
-        $this->setSwapTag("status", true);
-        $this->setSwapTag("message", "Package updated");
+        return true;
     }
 }
