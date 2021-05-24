c96e5a245f902ac6b7a0db9c07c261200c0bf58a
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-24 16:24:17 +0100

    Update js_on_select_api.php
    reformated page to not be so god dam long

--- src/App/Endpoint/View/Server/js_on_select_api.php
+++ src/App/Endpoint/View/Server/js_on_select_api.php
@@ -1,3 +1,6 @@
 <?php
 
-$this->output->addSwapTagString("html_js_onready", file_get_contents("" . ROOTFOLDER . "/App/Endpoint/View/Server/js_on_select_api.js"));
+$this->output->addSwapTagString(
+    "html_js_onready",
+    file_get_contents("" . ROOTFOLDER . "/App/Endpoint/View/Server/js_on_select_api.js")
+);
