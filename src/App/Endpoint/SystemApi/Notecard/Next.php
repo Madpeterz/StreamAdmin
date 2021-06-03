c8902b183b0a74d78ef183baab4b5092c1c90580
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-06-03 18:01:51 +0100

    Update Next.php

--- src/App/Endpoint/SystemApi/Notecard/Next.php
+++ src/App/Endpoint/SystemApi/Notecard/Next.php
@@ -16,89 +16,116 @@ use App\Template\SystemApiAjax;
 
 class Next extends SystemApiAjax
 {
+    protected $notecard = new Notecard();
+    protected $rental = new Rental();
+    protected $package = new Package();
+    protected $avatar = new Avatar();
+    protected $template = new Template();
+    protected $stream = new Stream();
+    protected $server = new Server();
+    protected $notice = new Notice();
+
+    protected $notecard_title = "";
+    protected $notecard_content = "";
+
+    protected function failedLoad(string $whyfailed): bool
+    {
+        $this->setSwapTag("message", "Unable to load: " . $whyfailed);
+        return false;
+    }
+
+    protected function loadContent(): bool
+    {
+        $swap_helper = new SwapablesHelper();
+        if ($this->notecard->getAsNotice() == false) {
+            $this->notecard_title = "Streamdetails for "
+            . $this->avatar->getAvatarName() . " port: "
+            . $this->stream->getPort() . "";
+            if ($this->template->getNotecarddetail() == null) {
+                $this->setSwapTag("message", "Selected template: " . $this->template->getName() . " is empty");
+                return false;
+            }
+            $this->notecard_content = $swap_helper->getSwappedText(
+                $this->template->getNotecarddetail(),
+                $this->avatar,
+                $this->rental,
+                $this->package,
+                $this->server,
+                $this->stream
+            );
+            return true;
+        }
+        $this->notecard_title = "Reminder for "
+        . $this->avatar->getAvatarName() . " port: "
+        . $this->stream->getPort() . "";
+        if ($this->notice->getNotecarddetail() == null) {
+            $this->setSwapTag("message", "Selected notice: " . $this->notice->getName() . " is empty");
+            return;
+        }
+        $this->notecard_content = $swap_helper->getSwappedText(
+            $this->notice->getNotecarddetail(),
+            $this->avatar,
+            $this->rental,
+            $this->package,
+            $this->server,
+            $this->stream
+        );
+    }
+
+    protected function loadData(): bool
+    {
+        if ($this->rental->loadID($this->notecard->getRentalLink()) == false) {
+            return $this->failedLoad("Rental");
+        }
+        if ($this->avatar->loadID($this->rental->getAvatarLink()) == false) {
+            return $this->failedLoad("Avatar");
+        }
+        if ($this->stream->loadID($this->rental->getStreamLink()) == false) {
+            return $this->failedLoad("Stream");
+        }
+        if ($this->server->loadID($this->stream->getServerLink()) == false) {
+            return $this->failedLoad("Server");
+        }
+        if ($this->package->loadID($this->stream->getPackageLink()) == false) {
+            return $this->failedLoad("Package");
+        }
+        if ($this->notecard->getAsNotice() == true) {
+            if ($this->template->loadID($this->package->getTemplateLink()) == false) {
+                return $this->failedLoad("Template");
+            }
+            return;
+        }
+        if ($this->notice->loadID($this->notecard->getNoticeLink()) == false) {
+            return $this->failedLoad("Template");
+        }
+        return true;
+    }
     public function process(): void
     {
-        $notecard = new Notecard();
-        $rental = new Rental();
-        $package = new Package();
-        $avatar = new Avatar();
-        $template = new Template();
-        $stream = new Stream();
-        $server = new Server();
-        $notice = new Notice();
-        $load_by = [
-        "rental" => ["notecard" => "rentalLink"],
-        "avatar" => ["rental" => "avatarLink"],
-        "stream" => ["rental" => "streamLink"],
-        "server" => ["stream" => "serverLink"],
-        "package" => ["stream" => "packageLink"],
-        ];
-        if ($notecard->getAsNotice() == false) {
-            $load_by["template"] = ["package" => "templateLink"];
-        } else {
-            $load_by["notice"] = ["rental" => "noticeLink"];
-        }
         $notecard_set = new NotecardSet();
         $notecard_set->loadNewest(1, [], [], "id", "ASC"); // lol loading oldest with newest command ^+^ hax
         if ($notecard_set->getCount() == 0) {
             $this->setSwapTag("status", true);
             $this->setSwapTag("message", "nowork");
+            return true;
+        }
+        $this->notecard = $notecard_set->getFirst();
+        if ($this->loadData() == false) {
             return;
         }
-        $notecard = $notecard_set->getFirst();
-        foreach ($load_by as $objectName => $value) {
-            foreach ($value as $source => $linkon) {
-                $object = $$objectName;
-                $loadfromobject = $$source;
-                $loadfromfunction = "get" . ucfirst($linkon);
-                if ($object->loadID($loadfromobject->$loadfromfunction()) == false) {
-                    $this->setSwapTag("message", "Unable to load object: 
-                    " . $source . " using " . $loadfromfunction);
-                    return;
-                }
-            }
+        if ($this->loadContent() == false) {
+            return;
         }
-        $notecard_title = "";
-        $notecard_content = "";
-        $swap_helper = new SwapablesHelper();
-        if ($notecard->getAsNotice() == false) {
-            $notecard_title = "Streamdetails for " . $avatar->getAvatarName() . " port: " . $stream->getPort() . "";
-            if ($template->getNotecarddetail() == null) {
-                $this->setSwapTag("message", "Selected template: " . $template->getName() . " is empty");
-                return;
-            }
-            $notecard_content = $swap_helper->getSwappedText(
-                $template->getNotecarddetail(),
-                $avatar,
-                $rental,
-                $package,
-                $server,
-                $stream
-            );
-        } else {
-            $notecard_title = "Reminder for " . $avatar->getAvatarName() . " port: " . $stream->getPort() . "";
-            if ($notice->getNotecarddetail() == null) {
-                $this->setSwapTag("message", "Selected notice: " . $notice->getName() . " is empty");
-                return;
-            }
-            $notecard_content = $swap_helper->getSwappedText(
-                $notice->getNotecarddetail(),
-                $avatar,
-                $rental,
-                $package,
-                $server,
-                $stream
-            );
-        }
-        $remove_status = $notecard->removeEntry();
+
+        $remove_status = $this->notecard->removeEntry();
         if ($remove_status["status"] == false) {
             $this->setSwapTag("message", "Unable to remove old entry");
             return;
         }
         $this->setSwapTag("status", true);
         $this->setSwapTag("message", "ok");
-        $this->setSwapTag("AvatarUUID", $avatar->getAvatarUUID());
-        $this->setSwapTag("NotecardTitle", $notecard_title);
-        $this->setSwapTag("NotecardContent", $notecard_content);
+        $this->setSwapTag("AvatarUUID", $this->avatar->getAvatarUUID());
+        $this->setSwapTag("NotecardTitle", $this->notecard_title);
+        $this->setSwapTag("NotecardContent", $this->notecard_content);
     }
 }
