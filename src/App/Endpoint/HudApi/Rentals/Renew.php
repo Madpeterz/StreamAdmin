3e3cf81470cf5491683291863c3a815d95a85cd5
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-06-21 23:51:00 +0100

    Update Renew.php

--- src/App/Endpoint/HudApi/Rentals/Renew.php
+++ src/App/Endpoint/HudApi/Rentals/Renew.php
@@ -3,6 +3,7 @@
 namespace App\Endpoint\HudApi\Rentals;
 
 use App\Endpoint\SecondLifeApi\Renew\Renewnow;
+use App\Helpers\ResellerHelper;
 use App\Template\SecondlifeHudAjax;
 use YAPF\InputFilter\InputFilter;
 
@@ -24,7 +25,21 @@ class Renew extends SecondlifeHudAjax
             $this->setSwapTag("message", "Failed - Invaild SL transaction ID");
             return;
         }
+        $reseller_helper = new ResellerHelper();
+        $get_reseller_status = $reseller_helper->loadOrCreate(
+            $this->slconfig->getOwnerAvatarLink(),
+            true,
+            0
+        );
+        if ($get_reseller_status == false) {
+            $this->load_ok = false;
+            $this->setSwapTag("message", "Unable to load reseller");
+            return;
+        }
         $renew = new Renewnow();
+        $renew->setReseller($reseller_helper->getReseller());
+        $renew->setOwnerOverride(true);
         $renew->process($this->Object_OwnerAvatar, $SLtransactionUUID);
+        $this->output = $renew->getOutputObject();
     }
 }
