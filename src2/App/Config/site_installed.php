de7700107602ed504e84c9fbe88b021fde650f59
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-07-16 17:48:55 +0100

    told you it would be back

--- src2/App/Config/site_installed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-$site_theme = "streamadminr5";
-$site_lang = "en";
-$template_parts["html_title"] = " Page ";
-$template_parts["html_title_after"] = "streamadmin test units";
-$template_parts["url_base"] = "http://localhost/";
