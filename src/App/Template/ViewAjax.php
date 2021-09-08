49d96036c5148eae3555c50a336f041cbd81ea3c
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-09-09 00:18:19 +0100

    add cleaner ok and failed handeler

--- src/App/Template/ViewAjax.php
+++ src/App/Template/ViewAjax.php
@@ -20,4 +20,17 @@ abstract class ViewAjax extends View
         $this->forceSave();
         $this->output->renderAjax();
     }
+    protected function ok(string $message): void
+    {
+        $this->setMessage($message, true);
+    }
+    protected function failed(string $message): void
+    {
+        $this->setMessage($message, false);
+    }
+    protected function setMessage(string $message, bool $status): void
+    {
+        $this->setSwapTag("status", $status);
+        $this->setSwapTag("message", $message);
+    }
 }
