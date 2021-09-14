9ac3dd99ad20b64b9df504954ee8b17e1dcd8c38
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-09-14 16:12:39 +0100

    add events outbox viewer

---
+++ src/App/Endpoint/View/Outbox/Events.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Endpoint\View\Outbox;
+
+use App\R7\Set\EventsqSet;
+
+class Events extends View
+{
+    public function process(): void
+    {
+        $this->output->addSwapTagString("page_title", " Unsent custom events Q");
+        $table_head = ["id","Name","Datetime"];
+        $table_body = [];
+        $eventqset = new EventsqSet();
+        $eventqset->limitFields(["eventName","eventUnixtime"]);
+        $eventqset->loadAll();
+        foreach ($eventqset as $eventq) {
+            $table_body[] = [
+                $eventq->getId(),
+                $eventq->getEventName(),
+                date('d/m/Y @ G:i:s', $eventq->getEventUnixtime()),
+            ];
+        }
+        $this->setSwapTag("page_content", $this->renderDatatable($table_head, $table_body));
+    }
+}
