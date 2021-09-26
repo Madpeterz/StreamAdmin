678df0e03f4df62a10d66c244568aa3b3686b7e5
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-09-27 00:06:59 +0100

    update sql

--- src/App/Versions/sql/1.0.2.2.sql
+++ src/App/Versions/sql/1.0.2.2.sql
@@ -8,3 +8,23 @@ ALTER TABLE `textureconfig` DROP `treevendWaiting`;
 ALTER TABLE `treevender` 
     ADD `textureWaiting` VARCHAR(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000' AFTER `name`, 
     ADD `textureInuse` VARCHAR(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000' AFTER `textureWaiting`;
+
+CREATE TABLE `datatable` (
+  `id` int(11) NOT NULL,
+  `col` int(11) NOT NULL DEFAULT 0,
+  `cols` text NOT NULL,
+  `name` text NOT NULL,
+  `dir` text NOT NULL DEFAULT 'desc'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `datatable` ADD PRIMARY KEY(`id`);
+
+ALTER TABLE `datatable` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `datatable` (`id`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '0', '0=id,1=region,3=Percentage,4=Count Up,5=Count down', 'health', 'desc');
+ALTER TABLE `datatable` ADD `hideColZero` TINYINT(1) NOT NULL DEFAULT '1' AFTER `id`;
+INSERT INTO `datatable` (`id`, `hideColZero`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '1', '1', '0=id,1=Object,2=Last seen,5=Owner', 'Health / Detailed', 'desc');
+UPDATE `datatable` SET `name` = 'Health' WHERE `datatable`.`id` = 1;
+INSERT INTO `datatable` (`id`, `hideColZero`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '1', '0', '0=id,1=Rental UID,2=Avatar,3=Port,6=Renewals', 'Client / List', 'desc');
+INSERT INTO `datatable` (`id`, `hideColZero`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '1', '0', '0=id,1=Stream UID,2=Server,3=Port', 'Stream / List', 'desc');
+INSERT INTO `datatable` (`id`, `hideColZero`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '1', '0', '0=id,1=Package name,2=Sold,3=Need work,4=Ready', 'Streams / Package menu', 'desc');
+INSERT INTO `datatable` (`id`, `hideColZero`, `col`, `cols`, `name`, `dir`) VALUES (NULL, '1', '0', '0=id,1=Package UID,2=Name,4=Listeners,5=Days,6=Kbps,7=Cost', 'Packages / List', 'desc');
