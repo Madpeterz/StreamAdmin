d4b9b76434ef436cd07a04accdf1a069176f0c68
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-09-30 14:27:16 +0100

    update tests

---
+++ tests/BugReports/Issue72_test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace StreamAdminR7;
+
+use PHPUnit\Framework\TestCase;
+
+class Issue72 extends TestCase
+{
+    public function test_crontabClientAutoSuspend()
+    {       
+        if(defined("TESTING") == false) { 
+            define("TESTING",true);
+        }
+        global $_SERVER;
+        $_SERVER["argv"]["t"] = "ClientAutoSuspend";
+        include "src/App/CronJob/CronTab.php";
+        $this->assertSame('{"time":0,"ok":1,"failed":0,"ticks":1,"sleeped":0}',$this->getActualOutputForAssertion(),"Reply from crontab is not as we expect");
+    }
+    public function test_DetailsServer()
+    {       
+        if(defined("TESTING") == false) { 
+            define("TESTING",true);
+        }
+        global $_SERVER;
+        $_SERVER["argv"]["t"] = "DetailsServer";
+        include "src/App/CronJob/CronTab.php";
+        $this->assertSame('{"time":0,"ok":1,"failed":0,"ticks":1,"sleeped":0}',$this->getActualOutputForAssertion(),"Reply from crontab is not as we expect");
+    }
+    public function test_ApiRequestsServer()
+    {       
+        if(defined("TESTING") == false) { 
+            define("TESTING",true);
+        }
+        global $_SERVER;
+        $_SERVER["argv"]["t"] = "ApiRequests";
+        include "src/App/CronJob/CronTab.php";
+        $this->assertSame('{"time":0,"ok":1,"failed":0,"ticks":1,"sleeped":0}',$this->getActualOutputForAssertion(),"Reply from crontab is not as we expect");
+    }
+}
+
