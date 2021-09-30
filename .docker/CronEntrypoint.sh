65e923ba4aae8e9c5d18ca1677bb1b2638c76921
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-09-30 20:06:44 +0100

    Update CronEntrypoint.sh

--- .docker/CronEntrypoint.sh
+++ .docker/CronEntrypoint.sh
@@ -8,7 +8,9 @@ echo "
 * * * * * /usr/bin/php /srv/website/src/App/CronJob/CronTab.php -t ApiRequests
 " > scheduler.txt
 
+echo scheduler.txt
+
+echo "Starting up"
 # Start the magic
 crontab scheduler.txt
-touch /var/log/cron.log
-cron && tail -F /var/log/cron.log
+crond -f -l
