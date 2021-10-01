6ab98bbf30e6c2addfb04c925b90a98e459f9c3a
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-01 11:28:02 +0100

    Update RegionHelper.php

--- src/App/Helpers/RegionHelper.php
+++ src/App/Helpers/RegionHelper.php
@@ -7,23 +7,31 @@ use App\R7\Model\Region;
 class RegionHelper
 {
     protected $region = null;
+    protected $lastError = "";
     public function getRegion(): region
     {
         return $this->region;
     }
+    public function getLastError(): string
+    {
+        return $this->lastError;
+    }
     public function loadOrCreate(string $regionname): bool
     {
         $this->region = new Region();
-        if (strlen($regionname) > 3) {
-            if ($this->region->loadByField("name", $regionname) == true) {
-                return true;
-            } else {
-                $this->region = new Region();
-                $this->region->setName($regionname);
-                $save_status = $this->region->createEntry();
-                return $save_status["status"];
-            }
+        if (strlen($regionname) < 3) {
+            $this->lastError = "Region name to short";
+            return false;
         }
-        return false;
+        if ($this->region->loadByField("name", $regionname) == true) {
+            return true;
+        }
+        $this->region = new Region();
+        $this->region->setName($regionname);
+        $save_status = $this->region->createEntry();
+        if ($save_status["status"] == false) {
+            $this->lastError = $save_status["message"];
+        }
+        return $save_status["status"];
     }
 }
