8cdf10b904c93dc026a3d2ca20a5df6fb078269c
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-01 10:59:58 +0100

    Update CronTab.php

--- src/App/CronJob/CronTab.php
+++ src/App/CronJob/CronTab.php
@@ -11,7 +11,7 @@ include ROOTFOLDER . "/App/Framework/load.php";
 set_time_limit(57);
 
 $options = get_opts();
-if (array_key_exists("t", $options) == false) {
+if (array_key_exists("t:", $options) == false) {
     echo "task arg t is missing unable to continue: " . json_encode($options);
     return;
 }
