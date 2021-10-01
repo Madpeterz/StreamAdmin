a6794912545d499b79da0125dbb75d3f2f1faa9d
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-01 12:51:29 +0100

    Update Master.php

--- src/App/CronJob/Master/Master.php
+++ src/App/CronJob/Master/Master.php
@@ -5,6 +5,7 @@ namespace App\CronJob\Master;
 use App\Helpers\ObjectHelper;
 use App\Helpers\RegionHelper;
 use App\R7\Model\Avatar;
+use App\R7\Model\Objects;
 use App\R7\Model\Slconfig;
 
 abstract class Master
@@ -14,6 +15,13 @@ abstract class Master
     protected string $cronRunClass = "";
     protected int $groups = 15;
     protected ?int $lockMaxGroups = null;
+
+    protected ?Objects $myObject;
+
+    protected int $ticks = 0;
+    protected int $sleepTime = 0;
+    protected float $startMicrotime = 0;
+    protected float $endMicrotime = 0;
 
     public function __construct(int $forceSetGroups = 15)
     {
@@ -26,30 +34,102 @@ abstract class Master
         $this->process();
     }
 
-    protected function process(): void
+    protected function report(): void
+    {
+        $output = [
+            "task" => $this->cronName,
+            "ticks" => $this->ticks,
+            "sleep" => $this->sleepTime,
+            "startTime" => date("H:i:s", $this->startMicrotime),
+            "endTime" => date("H:i:s", $this->endMicrotime),
+            "totalTime" => date("i:s", $this->endMicrotime - $this->startMicrotime),
+        ];
+        echo json_encode($output) . "\n";
+    }
+
+    protected function splitLooper(): bool
+    {
+        $unixtimeNow = time();
+        $autoExitTime = time() + 50;
+        $spacer = 55 / $this->groups;
+        $exitNow = false;
+        $hadError = false;
+        $nextDelay = 0;
+        while ($exitNow == false) {
+            if ($nextDelay > 0) {
+                $this->sleepTime += $nextDelay;
+                sleep($nextDelay);
+            }
+            if (($this->ticks % 3) == 0) {
+                $this->save($hadError); // auto save
+            }
+            $startTaskTime = time();
+            $hadError = !$this->doTask();
+            $this->ticks++;
+            if ($hadError == true) {
+                $exitNow = true;
+            }
+            $timeTaken = time() - $startTaskTime;
+            if ((time() + $timeTaken) > $autoExitTime) {
+                $exitNow = true;
+            }
+            $nextDelay = $spacer - $timeTaken;
+            if ($nextDelay < 1) {
+                $nextDelay = 0;
+            }
+            if ($this->ticks >= $this->groups) {
+                $exitNow = true;
+            }
+        }
+        return $hadError;
+    }
+    protected function doTask(): bool
+    {
+        $task = new $this->cronRunClass();
+        $task->setOwnerOverride(true);
+        $task->process();
+        return $task->getOutputObject()->getSwapTagBool("status");
+    }
+    protected function save(bool $hadError = false): void
+    {
+        global $sql, $cache;
+        if ($hadError == false) {
+            $this->myObject->setLastSeen(time());
+            $updateObj = $this->myObject->updateEntry();
+            $hadError = $updateObj["status"];
+        }
+        if ($hadError == false) {
+            if ($cache != null) {
+                $cache->shutdown(); // push changes to cache now.
+            }
+            $sql->sqlSave(false); // push changes to DB now.
+            return;
+        }
+        if ($cache != null) {
+            $cache->purge(); // something is wrong purge the cache.
+            $sql->sqlRollBack(); // something is wrong undo any changes.
+        }
+    }
+    protected function startup(): bool
     {
         $regionHelper = new RegionHelper();
         if ($regionHelper->loadOrCreate("cronJob") == false) {
-            echo "Unable to load/create region";
-            return;
+            echo "Unable to load/create region:" . $regionHelper->getLastError();
+            return false;
         }
         $region = $regionHelper->getRegion();
 
         $slconfig = new Slconfig();
         if ($slconfig->loadID(1) == false) {
             echo "Unable to load system config:" . $slconfig->getLastErrorBasic();
-            return;
+            return false;
         }
 
         $avatar = new Avatar();
         if ($avatar->loadID($slconfig->getOwnerAvatarLink()) == false) {
             echo "Unable to load owner avatar:" . $avatar->getLastErrorBasic();
-            return;
+            return false;
         }
-        global $sql;
-        $sql->sqlSave();
-
-
 
         $objectHelper = new ObjectHelper();
         if (
@@ -63,61 +143,25 @@ abstract class Master
             ) == false
         ) {
             echo "Unable to load/create object:" . $objectHelper->getLastWhyFailed();
+            return false;
+        }
+        $this->myObject = $objectHelper->getObject();
+        return true;
+    }
+    protected function process(): void
+    {
+        echo "Starting task: " . $this->cronName . "\n";
+        $this->startMicrotime = microtime(true);
+        if ($this->startup() == false) {
             return;
         }
-
-        $ticks = 0;
-        $units = 50 / $this->groups;
-        $autoExit = time() + 50;
-        $cronTimeStart = time();
-        $sleepFor = 0;
-        $tasksFinishedOk = 0;
-        $tasksFinsihedFailed = 0;
-        $totalSleepTime = 0;
-        while (
-            ($ticks < $this->groups) &&
-            (time() < $autoExit)
-        ) {
-            $startTime = time();
-            if ($sleepFor != 0) {
-                $totalSleepTime += $sleepFor;
-                sleep($sleepFor);
-            }
-            if (($ticks % 3) == 0) {
-                $obj = $objectHelper->getObject();
-                $obj->setLastSeen(time());
-                $obj->updateEntry();
-            }
-
-            $task = new $this->cronRunClass();
-            $task->setOwnerOverride(true);
-            $task->process();
-            $statussql = $task->getOutputObject()->getSwapTagBool("status");
-            $tasksFinsihedFailed++;
-            if ($statussql == false) {
-                $task->getoutput();
-            }
-            if ($statussql == true) {
-                $tasksFinishedOk++;
-                $tasksFinsihedFailed--;
-            }
-            if (($statussql === false) || ($statussql === null)) {
-                $sql->flagError();
-            }
-            $sql->sqlSave();
-
-            $dif = time() - $startTime;
-            $sleepFor = ceil($units - $dif);
-            $ticks++;
+        $this->save();
+        $hadError = $this->splitLooper();
+        if ($hadError == true) {
+            return;
         }
-        $timeInCron = time() - $cronTimeStart;
-        $reply = [
-            "time" => $timeInCron,
-            "ok" => $tasksFinishedOk,
-            "failed" => $tasksFinsihedFailed,
-            "ticks" => $tasksFinsihedFailed + $tasksFinishedOk,
-            "sleeped" => $totalSleepTime,
-        ];
-        echo json_encode($reply);
+        $this->endMicrotime = microtime(true);
+        $this->save();
+        $this->report();
     }
 }
