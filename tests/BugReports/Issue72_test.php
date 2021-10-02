dcf8e4ed05ee09ac3d400ac511c2771c2beb7b65
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-02 21:34:16 +0100

    Update Issue72_test.php

--- tests/BugReports/Issue72_test.php
+++ tests/BugReports/Issue72_test.php
@@ -36,5 +36,15 @@ class Issue72 extends TestCase
         include "src/App/CronJob/CronTab.php";
         $this->assertStringContainsString('"ticks":1,"sleep":0',$this->getActualOutputForAssertion(),"Reply from crontab is not as we expect");
     }
+    public function test_DynamicNotecards()
+    {       
+        if(defined("TESTING") == false) { 
+            define("TESTING",true);
+        }
+        global $_SERVER;
+        $_SERVER["argv"]["t"] = "DynamicNotecards";
+        include "src/App/CronJob/CronTab.php";
+        $this->assertStringContainsString('"ticks":1,"sleep":0',$this->getActualOutputForAssertion(),"Reply from crontab is not as we expect");
+    }
 }
 
