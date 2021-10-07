ae057ec66f05dffd9da4c40c08f66052aa37ef0b
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-07 01:30:17 +0100

    Update installer.sql

--- Versions/installer.sql
+++ Versions/installer.sql
@@ -706,3 +706,25 @@ ALTER TABLE `transactions`
 ALTER TABLE `treevenderpackages`
   ADD CONSTRAINT `package_in_use_treevenderpackages` FOREIGN KEY (`packageLink`) REFERENCES `package` (`id`) ON UPDATE NO ACTION,
   ADD CONSTRAINT `treevender_in_use_treevenderpackages` FOREIGN KEY (`treevenderLink`) REFERENCES `treevender` (`id`) ON UPDATE NO ACTION;
+
+UPDATE `slconfig` SET `dbVersion` = '1.0.2.5' WHERE `slconfig`.`id` = 1;
+ALTER TABLE `notice` ADD `sendObjectIM` TINYINT(1) NOT NULL DEFAULT '1' AFTER `imMessage`;
+
+CREATE TABLE `rentalnoticeptout` ( 
+        `id` INT NOT NULL AUTO_INCREMENT , 
+        `rentalLink` INT NOT NULL , 
+        `noticeLink` INT NOT NULL , 
+        `enabled` TINYINT(1) NOT NULL DEFAULT '1' , 
+        PRIMARY KEY (`id`), 
+        INDEX (`rentalLink`), 
+        INDEX (`noticeLink`)
+) ENGINE = InnoDB;
+
+ALTER TABLE `rentalnoticeptout` 
+  ADD CONSTRAINT `table: rental notice optout - Rental in use` FOREIGN KEY (`rentalLink`) REFERENCES `rental`(`id`) ON DELETE RESTRICT ON UPDATE NO ACTION; 
+  
+ALTER TABLE `rentalnoticeptout` 
+  ADD CONSTRAINT `table: rental notice optout - Notice in use` FOREIGN KEY (`noticeLink`) REFERENCES `notice`(`id`) ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+ALTER TABLE `rentalnoticeptout` DROP `enabled`;
+
