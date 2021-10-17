4647010b3f42aa5cd0c36b3616357bec615b7758
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-17 18:00:17 +0100

    extra checks for stream remove
    when removing a stream it now checks for rentals before so it can give a nicer error message

--- src/App/Endpoint/Control/Stream/Remove.php
+++ src/App/Endpoint/Control/Stream/Remove.php
@@ -2,7 +2,9 @@
 
 namespace App\Endpoint\Control\Stream;
 
+use App\R7\Model\Rental;
 use App\R7\Model\Stream;
+use App\R7\Set\ApirequestsSet;
 use App\R7\Set\TransactionsSet;
 use App\Template\ViewAjax;
 use YAPF\InputFilter\InputFilter;
@@ -13,7 +15,6 @@ class Remove extends ViewAjax
     {
         $input = new InputFilter();
         $accept = $input->postString("accept");
-        $this->setSwapTag("redirect", "stream");
         if ($accept != "Accept") {
             $this->failed("Did not Accept");
             $this->setSwapTag("redirect", "stream/manage/" . $this->page . "");
@@ -24,6 +25,25 @@ class Remove extends ViewAjax
             $this->failed("Unable to find stream");
             return;
         }
+        $rental = new Rental();
+        $rental->loadByStreamLink($stream->getId());
+        if ($rental->getId() > 0) {
+            $this->failed("That stream is currently rented how did you even get here.");
+            return;
+        }
+        $api_requests = new ApirequestsSet();
+        if ($api_requests->loadByStreamLink($stream->getId()) == false) {
+            $this->failed("Unable to check for pending api requests attached to the stream");
+            return;
+        }
+        if ($api_requests->getCount() > 0) {
+            $this->failed(sprintf(
+                "There are %1\$s pending api requests attached to the stream",
+                $api_requests->getCount()
+            ));
+            return;
+        }
+
         $transaction_set = new TransactionsSet();
         $load_status = $transaction_set->loadByStreamLink($stream->getId());
         if ($load_status["status"] == false) {
@@ -47,6 +67,7 @@ class Remove extends ViewAjax
             $this->failed(sprintf("Unable to remove stream: %1\$s", $remove_status["message"]));
             return;
         }
+        $this->setSwapTag("redirect", "stream");
         $this->ok("Stream removed");
     }
 }
