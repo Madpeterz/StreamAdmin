72e538319a30cb96e5a8684ab77d7d0ad8e66676
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-18 01:32:19 +0100

    update vendor

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit089df43dfb6b992ea980bb2322f819a1::getLoader();
+return ComposerAutoloaderInit5dbc20c67c507df090069269d9006aaa::getLoader();
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,12 +7,19 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'tests\\' => array($baseDir . '/tests'),
+    'ZipStream\\' => array($vendorDir . '/maennchen/zipstream-php/src'),
     'YAPF\\' => array($vendorDir . '/madpeterz/yetonemorephpframework/src'),
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src', $vendorDir . '/psr/http-factory/src'),
     'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'Predis\\' => array($vendorDir . '/predis/predis/src'),
+    'PhpOffice\\PhpSpreadsheet\\' => array($vendorDir . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet'),
+    'MyCLabs\\Enum\\' => array($vendorDir . '/myclabs/php-enum/src'),
+    'Matrix\\' => array($vendorDir . '/markbaker/matrix/classes/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Complex\\' => array($vendorDir . '/markbaker/complex/classes/src'),
     'App\\' => array($baseDir . '/src/App'),
 );
