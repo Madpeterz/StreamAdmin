16969308e022d1913307cbb21804892221f0397f
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-18 01:32:55 +0100

    Create CronEntrypointAndExporter.sh

---
+++ .docker/CronEntrypointAndExporter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Streamadmin crontab container"
+
+# Setup a cron schedule
+echo "* * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=DetailsServer >/proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=ClientAutoSuspend >/proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=ApiRequests >/proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=DynamicNotecards >/proc/1/fd/1 2>/proc/1/fd/2
+1 * * * * .docker/Exporter.sh >/proc/1/fd/1 2>/proc/1/fd/2
+" > scheduler.txt
+
+echo "Pushing ENV values to cron system"
+env >> /etc/environment
+
+echo "Starting up"
+# Start the magic
+crontab scheduler.txt
+cron -f -l 2
