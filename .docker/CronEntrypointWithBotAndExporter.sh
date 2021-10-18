7ee288d1f479327ffead5ce99f4640111ddc16fb
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-18 03:43:16 +0100

    Update CronEntrypointWithBotAndExporter.sh

--- .docker/CronEntrypointWithBotAndExporter.sh
+++ .docker/CronEntrypointWithBotAndExporter.sh
@@ -7,7 +7,7 @@ echo "* * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=D
 * * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=ApiRequests >/proc/1/fd/1 2>/proc/1/fd/2
 * * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=DynamicNotecards >/proc/1/fd/1 2>/proc/1/fd/2
 * * * * * /usr/local/bin/php /srv/website/src/App/CronJob/CronTab.php -t=BotcommandQ >/proc/1/fd/1 2>/proc/1/fd/2
-1 * * * * /srv/website/.docker/Exporter.sh >/proc/1/fd/1 2>/proc/1/fd/2
+* 1 * * * /srv/website/.docker/Exporter.sh >/proc/1/fd/1 2>/proc/1/fd/2
 " > scheduler.txt
 
 echo "Pushing ENV values to cron system"
