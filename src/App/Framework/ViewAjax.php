55190524386df6da3dd18d31007db46ebf649be6
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-23 17:59:54 +0000

    Create ViewAjax.php

---
+++ src/App/Framework/ViewAjax.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Framework;
+
+use App\Config;
+use YAPF\Bootstrap\Template\ViewAjax as TemplateViewAjax;
+use YAPF\InputFilter\InputFilter;
+
+abstract class ViewAjax extends TemplateViewAjax
+{
+    protected Config $siteConfig;
+    protected InputFilter $input;
+    public function __construct(
+        bool $AutoLoadTemplate = true
+    ) {
+        global $system;
+        $this->input = new InputFilter();
+        $this->siteConfig = $system;
+        parent::__construct();
+    }
+}
