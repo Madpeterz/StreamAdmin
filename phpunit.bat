77b8457e14ecb91e7b0243664bb26263d803a7f3
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-23 17:58:38 +0000

    Update phpunit.bat

--- phpunit.bat
+++ phpunit.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-SET BIN_TARGET="C:/Users/Madpeter/AppData/Roaming/Composer/vendor/phpunit/phpunit/phpunit"
+SET BIN_TARGET="C:/Users/madpe/AppData/Roaming/Composer/vendor/phpunit/phpunit/phpunit"
 php "%BIN_TARGET%" %*
