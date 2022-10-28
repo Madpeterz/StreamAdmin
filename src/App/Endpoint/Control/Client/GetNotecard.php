ca05a24a1d44e7688321d95334a13f5b94ec38b1
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-10-28 23:32:38 +0100

    Create GetNotecard.php

---
+++ src/App/Endpoint/Control/Client/GetNotecard.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Endpoint\Control\Client;
+
+use App\Helpers\SwapablesHelper;
+use App\Models\Rental;
+use App\Template\ControlAjax;
+
+class GetNotecard extends ControlAjax
+{
+    public function process(): void
+    {
+        $rental = new Rental();
+        if ($rental->loadByRentalUid($this->siteConfig->getPage())->status == false) {
+            $this->failed("Unable to load rental");
+            return;
+        }
+        $package = $rental->relatedPackage()->getFirst();
+        $template = $package?->relatedTemplate()->getFirst();
+        $avatar = $rental->relatedAvatar()->getFirst();
+        $stream = $rental->relatedStream()->getFirst();
+        $server = $stream?->relatedServer()->getFirst();
+        $test = [
+            "package" => $package,
+            "template" => $template,
+            "avatar" => $avatar,
+            "stream" => $stream,
+            "server" => $server,
+        ];
+        if (in_array(null, $test) == true) {
+            foreach ($test as $tag => $object) {
+                if ($object == null) {
+                    $this->failed("Failed to load: " . $tag);
+                    break;
+                }
+            }
+            return;
+        }
+        $this->ok((new SwapablesHelper())->getSwappedText(
+            $template->getDetail(),
+            $avatar,
+            $rental,
+            $package,
+            $server,
+            $stream
+        ));
+    }
+}
