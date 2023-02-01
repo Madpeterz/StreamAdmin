f71919b43c7823cef69fa86ac2db822eb7eade72
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2023-02-01 23:01:02 +0000

    Update Db.php

--- src/App/Db.php
+++ src/App/Db.php
@@ -14,6 +14,7 @@ class Db extends ErrorLogging
     {
         if (getenv('DB_HOST') !== false) {
             // Live
+            $this->addError("Switching to live config");
             $this->dbHost = getenv('DB_HOST');
             $this->dbName = getenv('DB_DATABASE');
             $this->dbUser = getenv('DB_USERNAME');
