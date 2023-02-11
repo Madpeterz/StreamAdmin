e013c7fdccd8503afa2eb180dc5cae458a3abc56
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2023-02-11 22:11:48 +0000

    Update CronTab.php

--- src/App/CronTab.php
+++ src/App/CronTab.php
@@ -5,8 +5,8 @@ namespace App;
 use App\Switchboard\CronTab;
 use App\Config;
 
-define("ERRORCONSOLE", "yes");
-define("ERRORCONSOLEPRINT", "yes");
+#define("ERRORCONSOLE", "yes");
+#define("ERRORCONSOLEPRINT", "yes");
 
 if (defined("TESTING") == false) {
     chdir(__DIR__);
