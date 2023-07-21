f2b24b5221c2af47d7c48b45328b4b0ca19c092d
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2023-07-21 15:09:58 +0100

    more

--- .docker/CronEntrypointWithBotAndExporter.sh
+++ .docker/CronEntrypointWithBotAndExporter.sh
@@ -2,17 +2,17 @@
 echo "Streamadmin crontab container"
 
 # Setup a cron schedule
-echo "* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Detailsserver > > /dev/stdout 2>&1
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Dynamicnotecards > > /dev/stdout 2>&1
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq > > /dev/stdout 2>&1
-${EXPORT_CRON_TIME} /srv/website/.docker/Exporter.sh > > /dev/stdout 2>&1
+echo "* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Detailsserver > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Dynamicnotecards > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq > /proc/1/fd/1 2>/proc/1/fd/2
+${EXPORT_CRON_TIME} /srv/website/.docker/Exporter.sh > /proc/1/fd/1 2>/proc/1/fd/2
 " > scheduler.txt
 
 echo "Pushing ENV values to cron system"
 env >> /etc/environment
 
 echo "Doing startup Export"
-/srv/website/.docker/Exporter.sh > > /dev/stdout 2>&1
+/srv/website/.docker/Exporter.sh > /proc/1/fd/1 2>/proc/1/fd/2
 
 echo "Starting up"
 # Start the magic
