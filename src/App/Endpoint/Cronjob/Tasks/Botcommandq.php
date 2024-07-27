50175abe46f23795e8b13a5df287ae2061fdc245
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-07-28 00:43:21 +0100

    adjust cron and line endings

--- src/App/Endpoint/Cronjob/Tasks/Botcommandq.php
+++ src/App/Endpoint/Cronjob/Tasks/Botcommandq.php
@@ -19,80 +19,8 @@ class Botcommandq extends Master
     {
         parent::__construct();
         $this->taskClass = new Next();
-        $this->task = new Next();
         $this->objectType = "botcommandqserver";
         $this->taskNicename = "Bot commandQ crontask";
         $this->taskId = 3;
-    }
-
-    protected function doTask(): bool
-    {
-        if ($this->makeBotConfig() == false) {
-            return false;
-        }
-        if ($this->makeBotAvatar() == false) {
-            return false;
-        }
-        $this->task = new Next();
-        $this->task->setCronConnected();
-        $this->task->setOwnerOverride(true);
-        $this->task->attachBotConfig($this->botconfig);
-        if ($this->makeHTTPClient() == false) {
-            return false;
-        }
-        $this->task->attachBotAvatar($this->botavatar);
-        $this->task->process();
-        $this->taskClass = $this->task;
-        return true;
-    }
-
-    protected function makeHTTPClient(): bool
-    {
-        if ($this->httpClient != null) {
-            $this->task->attachHTTPClient($this->httpClient);
-            return true;
-        }
-        if ($this->httpClient == null) {
-            $this->httpClient = $this->task->makeHTTPClient();
-        }
-        if ($this->httpClient == null) {
-            $this->failed("Unable to get HTTP client");
-            return false;
-        }
-        $this->task->attachHTTPClient($this->httpClient);
-        return true;
-    }
-
-    protected function makeBotConfig(): bool
-    {
-        if ($this->botconfig != null) {
-            return true;
-        }
-        $this->botconfig = new Botconfig();
-        $this->botconfig->loadID(1);
-        if ($this->botconfig->isLoaded() == false) {
-            $this->botconfig = null;
-            $this->failed("error - Unable to load bot config");
-            return false;
-        }
-        if ($this->botconfig->getHttpMode() == false) {
-            $this->failed("HTTP not enabled for bot");
-            return false;
-        }
-        return true;
-    }
-    protected function makeBotAvatar(): bool
-    {
-        if ($this->botavatar != null) {
-            return true;
-        }
-        $this->botavatar = new Avatar();
-        $this->botavatar->loadID($this->botconfig->getAvatarLink());
-        if ($this->botavatar->isLoaded() == false) {
-            $this->botavatar = null;
-            $this->failed("Unable to create avatar linked to bot");
-            return false;
-        }
-        return true;
     }
 }
