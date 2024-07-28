8dfdd386c21b1544cc8e9b44e915f01af14da0ad
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-07-28 12:50:58 +0100

    Update CronEntrypoint.sh

--- .docker/CronEntrypoint.sh
+++ .docker/CronEntrypoint.sh
@@ -15,16 +15,16 @@ echo "
 * * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Dynamicnotecards -d=31 > /proc/1/fd/1 2>/proc/1/fd/2
 * * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Dynamicnotecards -d=41 > /proc/1/fd/1 2>/proc/1/fd/2
 * * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Dynamicnotecards -d=51 > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=1  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=7  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=13  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=19  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=25  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=31  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=37  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=43  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=49  > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=55  > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=1 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=7 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=13 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=19 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=25 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=31 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=37 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=43 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=49 > /proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/local/bin/php /srv/website/src/App/CronTab.php -t=Botcommandq -d=55 > /proc/1/fd/1 2>/proc/1/fd/2
 " > scheduler.txt
 
 echo "Pushing ENV values to cron system"
