b8081fc149bb8a490a54e1edef9e81bfd27035a7
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-09-25 12:31:10 +0100

    sql updates

--- Versions/installer.sql
+++ Versions/installer.sql
@@ -654,3 +654,4 @@ ALTER TABLE `treevenderpackages`
 ALTER TABLE `objects` ADD UNIQUE(`objectUUID`);
 
 UPDATE `slconfig` SET `dbVersion` = '2.0.1.0' WHERE `slconfig`.`id` = 1;
+ALTER TABLE `botconfig` DROP `httpToken`;
