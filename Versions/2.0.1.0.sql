ee0b163314d54ab359fda5241a15b183e3070b5a
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-09-27 00:28:27 +0100

    sql updates - underway

---
+++ Versions/2.0.1.0.sql
@@ -0,0 +1,9 @@
+UPDATE `slconfig` SET `dbVersion` = '2.0.1.1' WHERE `slconfig`.`id` = 1;
+ALTER TABLE `slconfig` 
+ADD `limitStreams` TINYINT(1) NOT NULL DEFAULT '0' AFTER `clientsDisplayServer`, 
+ADD `limitTime` TINYINT(1) NOT NULL DEFAULT '0' AFTER `limitStreams`, 
+ADD `maxStreamTimeDays` INT NOT NULL DEFAULT '120' AFTER `limitTime`, 
+ADD `maxTotalStreams` INT NOT NULL DEFAULT '100' AFTER `maxStreamTimeDays`; 
+ALTER TABLE `package` 
+ADD `enforceCustomMaxStreams` TINYINT(1) NOT NULL DEFAULT '0' AFTER `enableGroupInvite`, 
+ADD `maxStreamsInPackage` INT NOT NULL DEFAULT '1' AFTER `enforceCustomMaxStreams`; 
